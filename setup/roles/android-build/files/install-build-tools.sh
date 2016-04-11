495aa52a37b102b8cb2d8758a72af204ecf3286e
mmihirk <mihir.khatwani@gmail.com>
2016-04-11 11:08:08 +0100

    Adding installation for build tools 23.0.1 for compilation of realm

--- setup/roles/android-build/files/install-build-tools.sh
+++ setup/roles/android-build/files/install-build-tools.sh
@@ -6,3 +6,12 @@ expect {
     eof
 }
 '
+
+expect -c '
+set timeout -1;
+spawn android update sdk --all --no-ui --filter build-tools-23.0.1;
+expect {
+    "Do you accept the license" { exp_send "y\r" ; exp_continue }
+    eof
+}
+'
