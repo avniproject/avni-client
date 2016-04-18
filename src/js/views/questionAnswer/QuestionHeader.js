317e9fb34497d4c8193361edb89d31321134de54
mmihirk <mihir.khatwani@gmail.com>
2016-04-18 19:43:50 +0100

    Fixed the proptype validation

--- src/js/views/questionAnswer/QuestionHeader.js
+++ src/js/views/questionAnswer/QuestionHeader.js
@@ -2,28 +2,28 @@ import React, { Component, StyleSheet, Text } from 'react-native';
 
 class QuestionHeader extends Component {
 
-  static propTypes = {
-    question: React.PropTypes.object.isRequired,
-  };
+    static propTypes = {
+        question: React.PropTypes.string.isRequired,
+    };
 
-  static styles = StyleSheet.create({
-    header: {
-      height: 100,
-      width: 100,
-      alignSelf: 'center',
-      textAlign: 'center',
-      color: '#333333',
-      marginBottom: 5,
-    },
-  });
+    static styles = StyleSheet.create({
+        header: {
+            height: 100,
+            width: 100,
+            alignSelf: 'center',
+            textAlign: 'center',
+            color: '#333333',
+            marginBottom: 5,
+        },
+    });
 
-  render() {
-    return (
-      <Text style={QuestionHeader.styles.header}>
-        {this.props.question}
-      </Text>
-    );
-  }
+    render() {
+        return (
+            <Text style={QuestionHeader.styles.header}>
+                {this.props.question}
+            </Text>
+        );
+    }
 }
 
 export default QuestionHeader;
