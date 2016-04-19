84899637edcd0719699c675a40bbdd956ad7a893
Pedro Antonio Souza Viegas <pedroasv@gmail.com>
2016-04-19 16:11:56 -0300

    using enzyme for component testing

--- __tests__/sanity-test.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-describe('sum', () => {
-  it('adds 1 + 1 to equal 3', () => {
-    expect(1+1).toBe(2);
-  });
-});
---
+++ src/test/views/questionAnswer/QuestionAnswerViewTest.js
@@ -0,0 +1,13 @@
+import React from 'react-native';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import QuestionAnswerView from '../../../js/views/questionAnswer/QuestionAnswerView';
+
+describe('<QuestionAnswerView />', () => {
+  it('should have `Numeric Question` as the first question', () => {
+    const context = { navigator: ()=> ({}) };
+    const wrapper = shallow(<QuestionAnswerView params={{diseaseName: 'stroke'}}/>, { context });
+    expect(wrapper.state('question')).to.equal('Numeric Question');
+  });
+});
+
