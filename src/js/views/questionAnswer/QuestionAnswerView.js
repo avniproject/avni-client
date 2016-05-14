e629dd65ca6702f58fdfd41da73d14f9437bda23
mmihirk <mihir.khatwani@gmail.com>
2016-05-14 13:49:34 +0100

    Adding react-redux and redux

--- src/js/views/questionAnswer/QuestionAnswerView.js
+++ src/js/views/questionAnswer/QuestionAnswerView.js
@@ -9,70 +9,70 @@ import AppState from "../../hack/AppState"
 @Path('/questionAnswer')
 class QuestionAnswerView extends Component {
 
-    static propTypes = {
-        params: React.PropTypes.object.isRequired
-    };
+  static propTypes = {
+    params: React.PropTypes.object.isRequired
+  };
 
-    static contextTypes = {
-        navigator: React.PropTypes.func.isRequired,
-        getService: React.PropTypes.func.isRequired
-    };
+  static contextTypes = {
+    navigator: React.PropTypes.func.isRequired,
+    getService: React.PropTypes.func.isRequired
+  };
 
-    constructor(props, context) {
-        super(props, context);
-        this.questionnaire = context.getService("questionnaireService").getQuestionnaire(AppState.conclusion.questionnaireName);
+  constructor(props, context) {
+    super(props, context);
+    this.questionnaire = context.getService("questionnaireService").getQuestionnaire(AppState.conclusion.questionnaireName);
+  }
+
+  toAnswer(questionAnswer) {
+    if (questionAnswer.questionDataType === 'Numeric')
+      return (<TextInput onChangeText={(text) => AppState.conclusion.currentAnswer = text}/>);
+    else
+      return (<AnswerList answers={this.questionAnswer.answers}/>);
+  };
+
+  previousButton(questionAnswer) {
+    if (!questionAnswer.isFirstQuestion)
+      return (<Text onPress={this.onPrevious}>Previous</Text>);
+  };
+
+  nextButton(questionAnswer) {
+    return (<Text onPress={this.onNext}>Next</Text>);
+  };
+
+  onPrevious = () => {
+    TypedTransition
+      .from(this)
+      .with({
+        questionNumber: this.props.params.questionNumber - 1
+      })
+      .to(QuestionAnswerView);
+  };
+
+  onNext = () => {
+    var typedTransition = TypedTransition.from(this);
+    if (this.questionAnswer.isLastQuestion) {
+      typedTransition.with().to(ConclusionView);
+    } else {
+      typedTransition.with({
+          questionNumber: this.props.params.questionNumber + 1
+        })
+        .to(QuestionAnswerView);
     }
+  };
 
-    toAnswer(questionAnswer) {
-        if (questionAnswer.questionDataType === 'Numeric')
-            return (<TextInput onChangeText={(text) => AppState.conclusion.currentAnswer = text} />);
-        else
-            return (<AnswerList answers={this.questionAnswer.answers}/>);
-    };
-
-    previousButton(questionAnswer) {
-        if (!questionAnswer.isFirstQuestion)
-            return (<Text onPress={this.onPrevious}>Previous</Text>);
-    };
-
-    nextButton(questionAnswer) {
-        return (<Text onPress={this.onNext}>Next</Text>);
-    };
-
-    onPrevious = () => {
-        TypedTransition
-            .from(this)
-            .with({
-                questionNumber: this.props.params.questionNumber - 1
-            })
-            .to(QuestionAnswerView);
-    };
-
-    onNext = () => {
-        var typedTransition = TypedTransition.from(this);
-        if (this.questionAnswer.isLastQuestion) {
-            typedTransition.with().to(ConclusionView);
-        } else {
-                typedTransition.with({
-                    questionNumber: this.props.params.questionNumber + 1
-                })
-                .to(QuestionAnswerView);            
-        }
-    };
-
-    render() {
-        this.questionnaire.setQuestionIndex(this.props.params.questionNumber);
-        this.questionAnswer = this.questionnaire.currentQuestion();
-        AppState.conclusion.currentQuestion = this.questionAnswer.question;
-        return (
-            <View>
-                <Question question={this.questionAnswer.question}/>
-                {this.toAnswer(this.questionAnswer)}
-                {this.previousButton(this.questionAnswer)}
-                {this.nextButton(this.questionAnswer)}
-            </View>
-        );
-    }
+  render() {
+    this.questionnaire.setQuestionIndex(this.props.params.questionNumber);
+    this.questionAnswer = this.questionnaire.currentQuestion();
+    AppState.conclusion.currentQuestion = this.questionAnswer.question;
+    return (
+      <View>
+        <Question question={this.questionAnswer.question}/>
+        {this.toAnswer(this.questionAnswer)}
+        {this.previousButton(this.questionAnswer)}
+        {this.nextButton(this.questionAnswer)}
+      </View>
+    );
+  }
 }
 
 export default QuestionAnswerView;
