91d05946941d8f501a674cbcd45479c1e884ccb5
mihirk <mihir.khatwani@gmail.com>
2016-07-13 13:26:46 +0530

    Removing unused DSL code

--- src/js/framework/DSL.js
+++
@@ -1,2 +0,0 @@
-export {greaterThan, lessThanAndGreaterThan, lessThan, No, stringComparison, Yes} from './dsl/matchers';
-export {ask, end, numeric, options, when} from './dsl/keywords';
--- src/js/models/End.js
+++
@@ -1,9 +0,0 @@
-import AbstractQuestion from './AbstractQuestion';
-
-class End extends AbstractQuestion {
-  end() {
-    return true;
-  }
-}
-
-export default End;
