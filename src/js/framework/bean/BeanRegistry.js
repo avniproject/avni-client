46e5740b243ea2fde1a60b1a9c2ac926f731fe66
mihirk <mihir.khatwani@gmail.com>
2016-07-13 13:19:44 +0530

    Beanregistry refactoring

--- src/js/framework/bean/BeanRegistry.js
+++ src/js/framework/bean/BeanRegistry.js
@@ -1,12 +1,13 @@
 import Registry from '../Registry';
 
 class BeanRegistry extends Registry {
-    init(db, beanStore) {
-        return new Map(Array.from(this.entities).map(([name, bean]) => [name, new bean(db, beanStore)]));
+    constructor() {
+        super();
+        this.getBean = this.getEntity;
     }
 
-    getBean(name) {
-        return this.getEntity(name);
+    init(db, beanStore) {
+        return new Map(Array.from(this.entities).map(([name, bean]) => [name, new bean(db, beanStore)]));
     }
 }
 
