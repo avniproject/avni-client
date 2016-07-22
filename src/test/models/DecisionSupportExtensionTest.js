bd7d94ceea5b5db42775ccb0b90385865e0e660a
mihirk <mihir.khatwani@gmail.com>
2016-07-23 01:42:22 +0530

    Removing test which is not valid anymore

--- src/test/models/DecisionSupportExtensionTest.js
+++
@@ -1,9 +0,0 @@
-import {expect} from 'chai';
-import DecisionSupportExtension from "../../js/models/DecisionSupportExtension";
-
-describe('Decision Support Extension', () => {
-    it('Module Name', () => {
-        expect(new DecisionSupportExtension("Stroke Screening").functionName).to.equal("Stroke_Screening_getDecision");
-        expect(new DecisionSupportExtension("StrokeScreening").functionName).to.equal("StrokeScreening_getDecision");
-    });
-});
