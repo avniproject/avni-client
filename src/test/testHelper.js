f2dd0ab26a7e4945305aaea3c965795920106d9c
mihirk <mihir.khatwani@gmail.com>
2016-08-01 15:06:11 +0530

    Removing react native mock

--- src/test/testHelper.js
+++ src/test/testHelper.js
@@ -3,18 +3,3 @@ var RN = require('react-native');
 
 var mockFN = function () {
 };
-
-RN.NativeModules['RNFSManager'] = {
-    readDir: mockFN,
-    exists: mockFN,
-    stat: mockFN,
-    readFile: mockFN,
-    writeFile: mockFN,
-    moveFile: mockFN,
-    unlink: mockFN,
-    mkdir: mockFN,
-    downloadFile: mockFN,
-    pathForBundle: mockFN,
-    getFSInfo: mockFN
-};
-
