8c0c738327333645ffd3e23483d4a5c3632f2533
mihirk <mihir.khatwani@gmail.com>
2016-08-04 15:26:19 +0530

    Adding conditional android install

--- setup/roles/android-build/files/install_android_libs.sh
+++ setup/roles/android-build/files/install_android_libs.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -ex
-echo y | android update sdk --no-ui --all --filter android-23
-echo y | android update sdk --no-ui --all --filter extra-android-m2repository
-echo y | android update sdk --no-ui --all --filter tools
-echo y | android update sdk --no-ui --all --filter platform-tools
-echo y | android update sdk --no-ui --all --filter build-tools-23.0.1
-echo y | android update sdk --no-ui --all --filter build-tools-23.0.3
-echo y | android update sdk --no-ui --all --filter sys-img-x86_64-android-23
-echo y | android update sdk --no-ui --all --filter sys-img-x86-android-23
-echo y | android update sdk --no-ui --all --filter extra-google-m2repository
+
+INITIALIZATION_FILE="$ANDROID_HOME/.initialized-dependencies-$(git log -n 1 --format=%h -- $0)"
+
+if [ ! -e ${INITIALIZATION_FILE} ]; then
+    echo y | android update sdk --no-ui --all --filter android-23
+    echo y | android update sdk --no-ui --all --filter extra-android-m2repository
+    echo y | android update sdk --no-ui --all --filter tools
+    echo y | android update sdk --no-ui --all --filter platform-tools
+    echo y | android update sdk --no-ui --all --filter build-tools-23.0.1
+    echo y | android update sdk --no-ui --all --filter build-tools-23.0.3
+    echo y | android update sdk --no-ui --all --filter sys-img-x86_64-android-23
+    echo y | android update sdk --no-ui --all --filter sys-img-x86-android-23
+    echo y | android update sdk --no-ui --all --filter extra-google-m2repository
+    touch ${INITIALIZATION_FILE}
+fi
