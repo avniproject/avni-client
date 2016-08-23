3d4fad1ba930ba9a6fe65eb68b5e205f7e47a818
mihirk <mihir.khatwani@gmail.com>
2016-08-24 02:39:52 +0530

    Fixing Spinner Defaults

--- src/js/framework/view/AbstractComponent.js
+++ src/js/framework/view/AbstractComponent.js
@@ -6,12 +6,13 @@ class AbstractComponent extends Component {
     constructor(props, context) {
         super(props, context);
         this.renderComponent = this.renderComponent.bind(this);
+        this.spinnerDefaults = Map({color: 'white', size: 'small'});
     }
 
     static styles = StyleSheet.create({
         spinner: {
             justifyContent: 'center',
-            alignSelf: 'center'
+            alignSelf: 'center',
         }
     });
 
@@ -25,7 +26,8 @@ class AbstractComponent extends Component {
         this.context.getStore().dispatch({"type": action});
     }
 
-    renderComponent(loading, component, color = "white", size = "small") {
+    renderComponent(loading, component, color = this.spinnerDefaults.get("color"),
+                    size = this.spinnerDefaults.get("size")) {
         if (loading) return (
             <ActivityIndicator style={AbstractComponent.styles.spinner} color={color} size={size}/>);
         return component;
