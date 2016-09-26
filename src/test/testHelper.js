d90ac7ad4a4d62f5ac50c52eee7da61befab3b4e
mihirk <mihir.khatwani@gmail.com>
2016-09-26 23:36:44 +0530

    Downloading the files and moving delete button to the settings view

--- src/test/testHelper.js
+++ src/test/testHelper.js
@@ -1,12 +1,15 @@
 var RN = require('react-native');
 
 RN.NativeModules['RNI18n'] = {'locale': 'en_GB'};
+RN.NativeModules['RNFetchBlob'] = {'locale': 'en_GB'};
 
 require('babel-core/register')({
     ignore: function (filename) {
         if (filename.indexOf("node_modules") === -1) {
             return false;
         } else if (filename.indexOf("node_modules") !== -1 && filename.indexOf("react-native-i18n") !== -1) {
+            return false;
+        } else if (filename.indexOf("node_modules") !== -1 && filename.indexOf("react-native-fetch-blob") !== -1) {
             return false;
         }
         return true;
