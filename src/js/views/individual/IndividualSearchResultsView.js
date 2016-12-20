64ac7eb4464591382335ecc221e47a209216d5fa
Arjun <arjunk@thoughtworks.com>
2016-12-20 11:35:53 +0530

    Arjun : Search Result page

--- src/js/views/individual/IndividualSearchResultsView.js
+++ src/js/views/individual/IndividualSearchResultsView.js
@@ -1,14 +1,14 @@
-import {View, StyleSheet, ListView, ScrollView, TouchableNativeFeedback} from 'react-native';
-import React, {Component} from 'react';
 import AbstractComponent from '../../framework/view/AbstractComponent';
+import {View} from 'react-native';
+import React from 'react';
 import Path from "../../framework/routing/Path";
 import {GlobalStyles} from "../primitives/GlobalStyles";
 import MessageService from "../../service/MessageService";
-import AppHeader from "../primitives/AppHeader";
 import TypedTransition from "../../framework/routing/TypedTransition";
 import IndividualEncounterView from "./IndividualEncounterView";
-import {Container, Content, List, ListItem, Thumbnail, Grid, Row, Col, Text, Button} from 'native-base';
-import Individual from "../../models/Individual"
+import {Container, Content, List, ListItem, Thumbnail, Grid, Row, Col, Text, Button, Header, Title, Icon} from 'native-base';
+import Individual from "../../models/Individual";
+import moment from "moment";
 
 @Path('/individualSearchResults')
 class IndividualSearchResultsView extends AbstractComponent {
@@ -46,29 +46,68 @@ class IndividualSearchResultsView extends AbstractComponent {
             return (<View/>);
     }
 
+    renderProgram(program, index){
+        //TODO this code to go away when association of colour is done with programs
+        var colour = "#f6a623";
+        if (index % 3 === 0)
+        {colour = "#4990e2"}
+        else if (index % 3 === 1)
+        { colour = "#4caf50"}
+
+
+
+        return (
+            <Button disabled  style={{marginLeft: 8, width: 74, height: 22, backgroundColor: '#f6a623'}}>{program.name}</Button>
+        );
+    }
+
+    getImage(individual){
+       if (individual.gender.name === 'Male'){
+           if (moment().diff(individual.dateOfBirth, 'years') > 30){
+               return <Thumbnail size={68}
+                                 source={require("../../../../android/app/src/main/res/mipmap-mdpi/narendra_modi.png")}/>
+           }
+           else {
+               return <Thumbnail size={68}
+                                 source={require("../../../../android/app/src/main/res/mipmap-mdpi/arvind_kejriwal.jpg")}/>
+           }
+       }
+       else if (individual.gender.name === 'Female'){
+           return <Thumbnail size={68}
+                             source={require("../../../../android/app/src/main/res/mipmap-mdpi/mamta.jpg")}/>
+       }
+
+    }
+
+
     render() {
-        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        const dsClone = ds.cloneWithRows(this.props.params.searchResults);
         return (
             <Container>
+                <Header style={{backgroundColor: '#212121'}}>
+                    <Button transparent onPress={() => {TypedTransition.from(this).goBack()}}>
+                        <Icon name='ios-arrow-back' />
+                    </Button>
+                    <Title>{this.I18n.t("SearchResults")}</Title>
+                </Header>
                 <Content>
                     <List dataArray={this.props.params.searchResults}
                           renderRow={(item) =>
-                              <ListItem>
+                              <ListItem style={{backgroundColor: '#f7f7f7', marginLeft: 0, paddingLeft: 17, padding: 17}}>
                                   <Grid>
                                       <Col size={1.5}>
-                                          <Thumbnail size={68}
-                                                     source={require('../../../../android/app/src/main/res/mipmap-mdpi/arvind_kejriwal.jpg')}/>
+                                          {this.getImage(item)}
                                       </Col>
                                       <Col size={5}>
-                                          <Row><Text>{item.name}</Text></Row>
+                                          <Row><Text style={{fontSize: 24}}>{item.name}</Text></Row>
                                           <Row>
-                                              <Text note>{item.gender.name + " | " + Individual.getDisplayAge(item)}</Text>
+                                              <Text style={{fontSize: 14}} note>{item.gender.name}</Text>
+                                              <Text style={{paddingLeft: 8, paddingRight: 8}}>|</Text>
+                                              <Text style={{fontSize: 14}} note>{Individual.getDisplayAge(item)}</Text>
                                           </Row>
                                       </Col>
                                       <Col size={2}>
-                                          <Row style={{justifyContent: 'flex-end'}}><Text>{item.lowestAddressLevel.title}</Text></Row>
-                                          <Row style={{justifyContent: 'flex-end'}}><Button disabled  style={{width: 74, height: 22, backgroundColor: '#f6a623'}}>Nutrition</Button><Button disabled  style={{width: 74, height: 22, backgroundColor: '#4990e2'}}>Diabetes</Button></Row>
+                                          <Row style={{justifyContent: 'flex-end'}}><Text style={{fontSize: 24}}>{item.lowestAddressLevel.title}</Text></Row>
+                                          <Row style={{justifyContent: 'flex-end'}}>{item.enrolments.map((enrolment, index) => this.renderProgram(enrolment.program, index))}</Row>
                                       </Col>
                                   </Grid>
                               </ListItem>
