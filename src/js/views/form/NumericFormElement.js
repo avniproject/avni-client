a6c642481a6ee08f3560a83204a5c29cfe06937a
Vivek Singh <petmongrels@gmail.com>
2017-01-23 18:38:20 +0530

    No need for multiselect checks in numericformelement also

--- src/js/views/form/NumericFormElement.js
+++ src/js/views/form/NumericFormElement.js
@@ -1,14 +1,8 @@
 import {View, StyleSheet} from "react-native";
 import React, {Component} from "react";
 import AbstractComponent from "../../framework/view/AbstractComponent";
-import _ from "lodash";
-import {
-    Text, Button, Content, CheckBox, Grid, Col, Row, Container, Header, Title, Icon, InputGroup,
-    Input, Radio
-} from "native-base";
-import DynamicGlobalStyles from '../primitives/DynamicGlobalStyles';
-import MultiSelectFormElement from './MultiSelectFormElement';
-import SingleSelectFormElement from './SingleSelectFormElement';
+import {Text, Row, InputGroup, Input} from "native-base";
+import DynamicGlobalStyles from "../primitives/DynamicGlobalStyles";
 
 
 class NumericFormElement extends AbstractComponent {
@@ -21,26 +15,17 @@ class NumericFormElement extends AbstractComponent {
     }
 
     render() {
-        if (this.props.element.concept.datatype === 'numeric') {
-            return (
-                <View>
-                    <Row style={{backgroundColor: '#ffffff', marginTop: 10, marginBottom: 10}}>
-                        <Text style={DynamicGlobalStyles.formElementLabel}>{this.props.element.name}</Text>
-                    </Row>
-                    <Row>
-                        <InputGroup style={{flex: 1}} borderType='underline'>
-                            <Input/>
-                        </InputGroup>
-                    </Row>
-
-                </View>)
-        }
-        else if(this.props.element.concept.datatype === 'multiselect') {
-            return <MultiSelectFormElement element={this.props.element}/>
-        }
-        else if (this.props.element.concept.datatype === 'singleselect') {
-            return <SingleSelectFormElement element={this.props.element} />
-        }
+        return (
+            <View>
+                <Row style={{backgroundColor: '#ffffff', marginTop: 10, marginBottom: 10}}>
+                    <Text style={DynamicGlobalStyles.formElementLabel}>{this.props.element.name}</Text>
+                </Row>
+                <Row>
+                    <InputGroup style={{flex: 1}} borderType='underline'>
+                        <Input/>
+                    </InputGroup>
+                </Row>
+            </View>);
     }
 }
 
