fcb3affc806cafad44d969b55cc8fb187c4cd2c0
Vivek Singh <petmongrels@gmail.com>
2017-01-26 18:00:37 +0530

    fixing build

--- src/test/common/WizardButtonsTest.js
+++ src/test/common/WizardButtonsTest.js
@@ -1,36 +1,23 @@
-import React from 'react';
-import {View, ListView, TouchableHighlight} from 'react-native';
-import {shallow} from 'enzyme';
-import {expect} from 'chai';
-import WizardButtons from '../../js/views/common/WizardButtons';
+import React from "react";
+import {View, ListView} from "react-native";
+import {shallow} from "enzyme";
+import {expect} from "chai";
+import WizardButtons from "../../js/views/common/WizardButtons";
 import TestContext from "../views/testframework/TestContext";
+import {Button} from "native-base";
 
 describe('WizardButtons View Test', () => {
     const context = new TestContext();
 
-    it('It should show no button if the button array passed is empty', () => {
-        const wrapper = shallow(<WizardButtons buttons={[]}/>, {context});
-        const buttons = wrapper.find(TouchableHighlight);
-        expect(buttons.length).to.be.equal(0);
-    });
-
-    it('It should show 1 button if button is passed', () => {
-        var expected = 0;
-        const wrapper = shallow(<WizardButtons
-            buttons={[{text: "First Button", func: ()=>expected = 1002, visible: true}]}/>, {context});
-        const buttons = wrapper.find(TouchableHighlight);
+    it('no previous', () => {
+        const wrapper = shallow(<WizardButtons previous={{func: ()=> {}, visible: false}} next={{func: ()=> {}, visible: true}}/>, {context});
+        const buttons = wrapper.find(Button);
         expect(buttons.length).to.be.equal(1);
     });
 
-    it('It should show 2 button if button is passed', () => {
-        var expected = 0;
-        const wrapper = shallow(<WizardButtons
-            buttons={[{text: "First Button", func: ()=>expected = 1002, visible: true}, {
-                text: "Second Button",
-                func: ()=>expected = 1002,
-                visible: true
-            }]}/>, {context});
-        const buttons = wrapper.find(TouchableHighlight);
+    it('both previous and next', () => {
+        const wrapper = shallow(<WizardButtons previous={{func: ()=> {}, visible: true}} next={{func: ()=> {}, visible: true}}/>, {context});
+        const buttons = wrapper.find(Button);
         expect(buttons.length).to.be.equal(2);
     });
 });
