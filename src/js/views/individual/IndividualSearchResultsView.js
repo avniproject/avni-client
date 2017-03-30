a7e75a54e33fbf8d1435caecc7bcc5899aee667f
Arjun <arjunk@thoughtworks.com>
2017-03-30 06:16:30 +0530

    Removed showing catchmentId on search result which was added just for debugging purpose

--- src/js/views/individual/IndividualSearchResultsView.js
+++ src/js/views/individual/IndividualSearchResultsView.js
@@ -106,7 +106,6 @@ class IndividualSearchResultsView extends AbstractComponent {
                                       </Col>
                                       <Col style={{width: DynamicGlobalStyles.resizeWidth(246)}}>
                                           <Row style={{justifyContent: 'flex-end'}}><Text style={{fontSize: 16}}>{item.lowestAddressLevel.name}</Text></Row>
-                                          <Row style={{justifyContent: 'flex-end'}}><Text style={{fontSize: 16}}>{item.catchmentId}</Text></Row>
                                           <Row
                                               style={{justifyContent: 'flex-end'}}>{_.filter(item.enrolments, (enrolment) => enrolment.isActive).map((enrolment, index) => this.renderProgram(enrolment.program, index))}</Row>
                                       </Col>
