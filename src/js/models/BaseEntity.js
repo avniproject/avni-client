51de1881452e615485a60d5495b7ca08a639f4fb
Vivek Singh <petmongrels@gmail.com>
2017-04-12 18:04:00 +0530

    fixed defect

--- src/js/models/BaseEntity.js
+++ src/js/models/BaseEntity.js
@@ -10,7 +10,7 @@ class BaseEntity {
         const existing = existingChildren.some((child) => {
             return newChild.uuid === child.uuid;
         });
-        if (existing)
+        if (!existing)
             existingChildren.push(newChild);
     }
 
