89bef93613b6e51e6807b027a5b83dc5d37b4fac
Charles <charlesj@gmail.com>
2017-08-16 14:49:58 +0530

    Updated Ansible scripts to factor in android-sdk package changes

--- install_android_libs.sh
+++ install_android_libs.sh
@@ -4,14 +4,16 @@ set -ex
 INITIALIZATION_FILE="$ANDROID_HOME/.initialized-dependencies-$(git log -n 1 --format=%h -- $0)"
 
 if [ ! -e ${INITIALIZATION_FILE} ]; then
-    echo y | android update sdk --no-ui --all --filter android-25
-    echo y | android update sdk --no-ui --all --filter extra-android-m2repository
-    echo y | android update sdk --no-ui --all --filter tools
-    echo y | android update sdk --no-ui --all --filter platform-tools
-    echo y | android update sdk --no-ui --all --filter build-tools-25.0.1
-    echo y | android update sdk --no-ui --all --filter build-tools-25.0.3
-    echo y | android update sdk --no-ui --all --filter sys-img-x86_64-android-25
-    echo y | android update sdk --no-ui --all --filter sys-img-x86-android-25
-    echo y | android update sdk --no-ui --all --filter extra-google-m2repository
+    sdkmanager "platforms;android-25"
+    sdkmanager "platforms;android-26"
+    sdkmanager "platforms;android-23"
+    sdkmanager "extras;android;m2repository"
+    sdkmanager "tools"
+    sdkmanager "platform-tools"
+    sdkmanager "build-tools;25.0.1"
+    sdkmanager "build-tools;25.0.3"
+    sdkmanager "system-images;android-25;google_apis;x86"
+    sdkmanager "system-images;android-25;google_apis;x86_64"
+    sdkmanager "extras;google;m2repository"
     touch ${INITIALIZATION_FILE}
 fi
