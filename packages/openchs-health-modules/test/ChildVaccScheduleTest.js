0b468d352f86bd78f8c148bafc21db80d085f2ae
Vivek Singh <petmongrels@gmail.com>
2017-09-14 20:57:36 +0530

    test started failing. import and require confusion

--- packages/openchs-health-modules/test/ChildVaccScheduleTest.js
+++ packages/openchs-health-modules/test/ChildVaccScheduleTest.js
@@ -1,6 +1,6 @@
 var expect = require('chai').expect;
 var assert = require('chai').assert;
-var getVaccSchedule = require('../health_modules/child/childVaccSchedule');
+import getVaccSchedule from '../health_modules/child/childVaccSchedule';
 
 describe('Create Child Vaccincation Schedule', function () {
     var progEnrolment;
@@ -31,4 +31,3 @@ describe('Create Child Vaccincation Schedule', function () {
         return date1.getFullYear() === date2.getFullYear() && date1.getMonth() === date2.getMonth() && date1.getDate() === date2.getDate();
     }
 });
-
