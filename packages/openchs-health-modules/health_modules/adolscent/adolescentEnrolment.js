6f8e35c9cbe401ea7fb46bd7b12d5d1857f9b16b
mihirk <mihir.khatwani@gmail.com>
2017-11-07 10:15:19 +0530

    Adding adolescent enrolment decision

--- packages/openchs-health-modules/health_modules/adolscent/adolescentEnrolment.js
+++ packages/openchs-health-modules/health_modules/adolscent/adolescentEnrolment.js
@@ -1,8 +1,37 @@
 import ComplicationsBuilder from "../rules/complicationsBuilder";
 import _ from "lodash";
 
-const getDecisions = (programEncounter) => {
-    return {enrolmentDecisions: [], encounterDecisions: [], registrationDecisions: []};
+
+const getDecisions = (programEnrolment, today) => {
+    let enrolmentDecisions = [];
+    const complicationsBuilder = new ComplicationsBuilder({
+        programEnrolment: programEnrolment,
+        complicationsConcept: 'Adolescent Vulnerabilities'
+    });
+
+    complicationsBuilder.addComplication("School dropout")
+        .when.valueInEntireEnrolment("Parents expired?").containsAnyAnswerConceptName("Father", "Mother")
+        .or
+        .when.valueInEntireEnrolment("Father's Addiction").containsAnyAnswerConceptName("Alcohol", "Tobacco")
+        .or
+        .when.valueInEntireEnrolment("Mother's Addiction").containsAnyAnswerConceptName("Alcohol", "Tobacco")
+        .or
+        .when.valueInEntireEnrolment("Chronic sickness in family").containsAnyAnswerConceptName("Diabetes", "Hypertension", "TB", "Cancer", "Sicklecell Disease", "Other")
+        .or
+        .when.valueInEntireEnrolment("Number of family members").greaterThan(6);
+
+    complicationsBuilder.addComplication("Addiction")
+        .when.valueInEntireEnrolment("Parents expired?").containsAnyAnswerConceptName("Father", "Mother")
+        .or
+        .when.valueInEntireEnrolment("Father's Addiction").containsAnyAnswerConceptName("Alcohol", "Tobacco")
+        .or
+        .when.valueInEntireEnrolment("Mother's Addiction").containsAnyAnswerConceptName("Alcohol", "Tobacco");
+
+    complicationsBuilder.addComplication("Addiction")
+        .when.valueInEntireEnrolment("Number of family members").greaterThan(6);
+
+    if (complicationsBuilder.hasComplications()) enrolmentDecisions.concat(complicationsBuilder.getComplications());
+    return {enrolmentDecisions: enrolmentDecisions, encounterDecisions: [], registrationDecisions: []};
 };
 
 export {getDecisions};
