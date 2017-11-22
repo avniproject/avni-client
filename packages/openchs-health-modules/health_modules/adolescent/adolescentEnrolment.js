8b2998a1149b0b39363c48680b93bc4ec1f0718b
mihirk <mihir.khatwani@gmail.com>
2017-11-22 18:50:58 +0530

    Adding adolescents rules again

--- packages/openchs-health-modules/health_modules/adolescent/adolescentEnrolment.js
+++ packages/openchs-health-modules/health_modules/adolescent/adolescentEnrolment.js
@@ -6,32 +6,32 @@ import {getNextScheduledVisits} from './adolescentVisitSchedule';
 const getDecisions = (programEnrolment, today) => {
     let enrolmentDecisions = [];
     console.log(programEnrolment);
-    // const vulnerabilitiesBuilder = new ComplicationsBuilder({
-    //     programEnrolment: programEnrolment,
-    //     complicationsConcept: 'Adolescent Vulnerabilities'
-    // });
-    //
-    // vulnerabilitiesBuilder.addComplication("School dropout")
-    //     .when.valueInEntireEnrolment("Parents expired?").containsAnyAnswerConceptName("Father", "Mother")
-    //     .or
-    //     .when.valueInEntireEnrolment("Father's Addiction").containsAnyAnswerConceptName("Alcohol", "Tobacco")
-    //     .or
-    //     .when.valueInEntireEnrolment("Mother's Addiction").containsAnyAnswerConceptName("Alcohol", "Tobacco")
-    //     .or
-    //     .when.valueInEntireEnrolment("Chronic sickness in family").containsAnyAnswerConceptName("Diabetes", "Hypertension", "TB", "Cancer", "Sicklecell Disease", "Other")
-    //     .or
-    //     .when.valueInEntireEnrolment("Number of family members").greaterThan(6);
-    //
-    // vulnerabilitiesBuilder.addComplication("Addiction")
-    //     .when.valueInEntireEnrolment("Parents expired?").containsAnyAnswerConceptName("Father", "Mother")
-    //     .or
-    //     .when.valueInEntireEnrolment("Father's Addiction").containsAnyAnswerConceptName("Alcohol", "Tobacco")
-    //     .or
-    //     .when.valueInEntireEnrolment("Mother's Addiction").containsAnyAnswerConceptName("Alcohol", "Tobacco");
-    //
-    // vulnerabilitiesBuilder.addComplication("Addiction")
-    //     .when.valueInEntireEnrolment("Number of family members").greaterThan(6);
-    // enrolmentDecisions.push(vulnerabilitiesBuilder.getComplications());
+    const vulnerabilitiesBuilder = new ComplicationsBuilder({
+        programEnrolment: programEnrolment,
+        complicationsConcept: 'Adolescent Vulnerabilities'
+    });
+
+    vulnerabilitiesBuilder.addComplication("School dropout")
+        .when.valueInEntireEnrolment("Parents expired?").containsAnyAnswerConceptName("Father", "Mother")
+        .or
+        .when.valueInEntireEnrolment("Father's Addiction").containsAnyAnswerConceptName("Alcohol", "Tobacco")
+        .or
+        .when.valueInEntireEnrolment("Mother's Addiction").containsAnyAnswerConceptName("Alcohol", "Tobacco")
+        .or
+        .when.valueInEntireEnrolment("Chronic sickness in family").containsAnyAnswerConceptName("Diabetes", "Hypertension", "TB", "Cancer", "Sicklecell Disease", "Other")
+        .or
+        .when.valueInEntireEnrolment("Number of family members").greaterThan(6);
+
+    vulnerabilitiesBuilder.addComplication("Addiction")
+        .when.valueInEntireEnrolment("Parents expired?").containsAnyAnswerConceptName("Father", "Mother")
+        .or
+        .when.valueInEntireEnrolment("Father's Addiction").containsAnyAnswerConceptName("Alcohol", "Tobacco")
+        .or
+        .when.valueInEntireEnrolment("Mother's Addiction").containsAnyAnswerConceptName("Alcohol", "Tobacco");
+
+    vulnerabilitiesBuilder.addComplication("Addiction")
+        .when.valueInEntireEnrolment("Number of family members").greaterThan(6);
+    enrolmentDecisions.push(vulnerabilitiesBuilder.getComplications());
     return {enrolmentDecisions: enrolmentDecisions, encounterDecisions: [], registrationDecisions: []};
 };
 
