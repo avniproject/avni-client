e703a78096ba0c56261dbfece30578a8e7d8fce4
mihirk <mihir.khatwani@gmail.com>
2017-12-06 13:22:34 +0530

    Updating package lock and some minor reformatting

--- packages/openchs-android/src/views/common/WizardButtons.js
+++ packages/openchs-android/src/views/common/WizardButtons.js
@@ -1,8 +1,8 @@
 import {View} from "react-native";
 import React, {Component} from "react";
 import {
-    Text, Button, Content, CheckBox, Grid, Col, Row, Container, Header, Title, Icon, InputGroup,
-    Input, Radio
+    Button
+
 } from "native-base";
 import AbstractComponent from "../../framework/view/AbstractComponent";
 import _ from 'lodash';
@@ -20,7 +20,7 @@ class WizardButtons extends AbstractComponent {
     };
 
     getButtonProps(buttonProps) {
-        var returnProps = buttonProps;
+        let returnProps = buttonProps;
         if (_.isNil(returnProps)) returnProps = {visible: false};
         if (!_.isNil(returnProps.label) && _.isNil(returnProps.visible)) returnProps.visible = true;
         if (_.isNil(returnProps.func)) returnProps.func = () => {
@@ -32,14 +32,21 @@ class WizardButtons extends AbstractComponent {
         const previousButton = this.getButtonProps(this.props.previous);
         const nextButton = this.getButtonProps(this.props.next);
         return (
-            <View style={this.appendedStyle({marginVertical: 30, justifyContent: 'space-between', flexDirection: 'row'})}>
-                {previousButton.visible ? <Button primary
-                                                  style={{flex: 0.5, backgroundColor: Colors.SecondaryActionButtonColor}}
-                                                  textStyle={{color: '#212121'}} onPress={() => previousButton.func()}>{previousButton.label}</Button> :
+            <View
+                style={this.appendedStyle({marginVertical: 30, justifyContent: 'space-between', flexDirection: 'row'})}>
+                {previousButton.visible ?
+                    <Button primary
+                            style={{
+                                flex: 0.5,
+                                backgroundColor: Colors.SecondaryActionButtonColor
+                            }}
+                            textStyle={{color: '#212121'}}
+                            onPress={() => previousButton.func()}>{previousButton.label}</Button> :
                     <View style={{flex: 0.5}}/>}
                 {nextButton.visible ?
                     <Button primary
-                            style={{flex: 0.5, marginLeft: 8}} onPress={() => nextButton.func()}>{nextButton.label}</Button> : <View style={{flex: 0.5}}/>}
+                            style={{flex: 0.5, marginLeft: 8}}
+                            onPress={() => nextButton.func()}>{nextButton.label}</Button> : <View style={{flex: 0.5}}/>}
             </View>
         );
     }
