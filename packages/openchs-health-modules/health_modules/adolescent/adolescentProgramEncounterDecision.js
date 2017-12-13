c986d397918f0130c3ab45db11c0fc57538cca84
Vinay Venu <ijkvinay@gmail.com>
2017-12-13 14:50:29 +0530

    Rename file

--- packages/openchs-health-modules/health_modules/adolescent/adolescentProgramEncounterDecision.js
+++ packages/openchs-health-modules/health_modules/adolescent/adolescentProgramEncounterDecision.js
@@ -1,10 +1,12 @@
 import FormFilterHelper from "../rules/FormFilterHelper";
-import EleventhAndTwlefthStandard from './formFilters/EleventhAndTwlefthStandard';
 import MonthlyEncounter from './formFilters/MonthlyEncounter';
 
+import EleventhAndTwelfthStandardFormHandler from './formFilters/EleventhAndTwelfthStandardFormHandler';
+
 const encounterTypeHandlerMap = new Map([
-    ['Adolescent Monthly Visit (11th and 12th std)', new EleventhAndTwlefthStandard()],
-    ['Monthly Visit', new MonthlyEncounter()]]);
+    ['Adolescent Monthly Visit (11th and 12th std)', new EleventhAndTwelfthStandardFormHandler()],
+    ['Monthly Visit', new MonthlyEncounter()]
+]);
 
 export function filterFormElements(programEncounter, formElementGroup, today) {
     let handler = encounterTypeHandlerMap.get(programEncounter.encounterType.name);
