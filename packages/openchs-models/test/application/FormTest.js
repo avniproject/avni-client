366b1115c350b79d1b3638cd935a033b01019c45
mihirk <mihir.khatwani@gmail.com>
2017-12-15 13:44:31 +0530

    Adding more tests for observation ordering

--- packages/openchs-models/test/application/FormTest.js
+++ packages/openchs-models/test/application/FormTest.js
@@ -37,6 +37,47 @@ describe('Form', () => {
         assert.equal(modifiedForm.findFormElement('b2'), undefined);
     });
 
+    it("Should return empty array if no observations", () => {
+        const form = EntityFactory.createForm('foo');
+        const formElementGroup1 = EntityFactory.createFormElementGroup('bar1', 1, form);
+        let bar1a1Concept = EntityFactory.createConcept("bar1a1Concept", "Numeric", "ad2856b6-ab33-46f9-a9f4-67f9d5dac09e");
+        formElementGroup1.addFormElement(EntityFactory.createFormElement('bar1a1', true, bar1a1Concept));
+        let bar1a2Concept = EntityFactory.createConcept("bar1a2Concept", "Numeric", "6127cdea-310a-4ae6-9af8-8bae7614c9fa");
+        formElementGroup1.addFormElement(EntityFactory.createFormElement('bar1a2', true, bar1a2Concept));
+
+        const formElementGroup2 = EntityFactory.createFormElementGroup('bar2', 2, form);
+        let bar2b1Concept = EntityFactory.createConcept("bar2b1Concept", "Numeric", "138a3a03-1a92-4b6c-ae4e-64b5af32dc74");
+        formElementGroup2.addFormElement(EntityFactory.createFormElement('bar2b1', true, bar2b1Concept));
+        let bar2b2Concept = EntityFactory.createConcept("bar2b2Concept", "Numeric", "955c0314-d02a-4e7b-a0a0-6a9d6189dcdd");
+        formElementGroup2.addFormElement(EntityFactory.createFormElement('bar2b2', true, bar2b2Concept));
+
+
+        let orderedObservations = form.orderObservations([]);
+        assert.equal(0, orderedObservations.length)
+    });
+
+    it("Should return extra obs array if no observations", () => {
+        const form = EntityFactory.createForm('foo');
+        const formElementGroup1 = EntityFactory.createFormElementGroup('bar1', 1, form);
+        let bar1a1Concept = EntityFactory.createConcept("bar1a1Concept", "Numeric", "ad2856b6-ab33-46f9-a9f4-67f9d5dac09e");
+        formElementGroup1.addFormElement(EntityFactory.createFormElement('bar1a1', true, bar1a1Concept));
+        let bar1a2Concept = EntityFactory.createConcept("bar1a2Concept", "Numeric", "6127cdea-310a-4ae6-9af8-8bae7614c9fa");
+        formElementGroup1.addFormElement(EntityFactory.createFormElement('bar1a2', true, bar1a2Concept));
+
+        const formElementGroup2 = EntityFactory.createFormElementGroup('bar2', 2, form);
+        let bar2b1Concept = EntityFactory.createConcept("bar2b1Concept", "Numeric", "138a3a03-1a92-4b6c-ae4e-64b5af32dc74");
+        formElementGroup2.addFormElement(EntityFactory.createFormElement('bar2b1', true, bar2b1Concept));
+        let bar2b2Concept = EntityFactory.createConcept("bar2b2Concept", "Numeric", "955c0314-d02a-4e7b-a0a0-6a9d6189dcdd");
+        formElementGroup2.addFormElement(EntityFactory.createFormElement('bar2b2', true, bar2b2Concept));
+
+        const extraObs = EntityFactory.createObservation(EntityFactory.createConcept("ExtraObs", "Numeric", "b86c0222-ae51-4ac7-97bc-ec2a4efc483b"));
+
+
+        let orderedObservations = form.orderObservations([extraObs]);
+        assert.equal(1, orderedObservations.length);
+        assert.equal("ExtraObs", orderedObservations[0].concept.name)
+    });
+
     it("Should order observations according to the form display order", () => {
         const form = EntityFactory.createForm('foo');
         const formElementGroup1 = EntityFactory.createFormElementGroup('bar1', 1, form);
