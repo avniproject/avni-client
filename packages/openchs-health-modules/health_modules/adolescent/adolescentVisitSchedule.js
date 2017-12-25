03b0067cbdf95142a7977af6e23e69bf29787d9e
mihirk <mihir.khatwani@gmail.com>
2017-12-25 16:33:26 +0530

    Removing comments on the encounter scheduling code

--- packages/openchs-health-modules/health_modules/adolescent/adolescentVisitSchedule.js
+++ packages/openchs-health-modules/health_modules/adolescent/adolescentVisitSchedule.js
@@ -8,52 +8,7 @@ const encounterSchedule = {
 };
 
 const getNextScheduledVisits = function (programEnrolment, today, currentEncounter) {
-
-    const encounters = [];
-
-    // const currentEncounters = programEnrolment.encounters;
-    //
-    // const addEncounter = function (baseDate, encounterType, name) {
-    //     if (_.encounterExists(currentEncounters, encounterType, name)) return;
-    //
-    //     let schedule = encounterSchedule[encounterType];
-    //     const earliestDate = _.addDays(baseDate, schedule.earliest);
-    //     const maxDate = _.addDays(baseDate, schedule.max);
-    //     encounters.push({
-    //         name: name,
-    //         encounterType: encounterType,
-    //         earliestDate: earliestDate,
-    //         maxDate: maxDate
-    //     });
-    //     return maxDate;
-    // };
-    // //Monthly Encounter
-    // let monthlyEncounterDueDate = addEncounter(today, "Monthly Visit", "Monthly Visit 1");
-    // monthlyEncounterDueDate = addEncounter(_.addDays(monthlyEncounterDueDate, 30), "Monthly Visit", "Monthly Visit 2");
-    // monthlyEncounterDueDate = addEncounter(_.addDays(monthlyEncounterDueDate, 30), "Monthly Visit", "Monthly Visit 3");
-    // monthlyEncounterDueDate = addEncounter(_.addDays(monthlyEncounterDueDate, 30), "Monthly Visit", "Monthly Visit 4");
-    // monthlyEncounterDueDate = addEncounter(_.addDays(monthlyEncounterDueDate, 30), "Monthly Visit", "Monthly Visit 5");
-    // monthlyEncounterDueDate = addEncounter(_.addDays(monthlyEncounterDueDate, 30), "Monthly Visit", "Monthly Visit 6");
-    // monthlyEncounterDueDate = addEncounter(_.addDays(monthlyEncounterDueDate, 30), "Monthly Visit", "Monthly Visit 7");
-    // monthlyEncounterDueDate = addEncounter(_.addDays(monthlyEncounterDueDate, 30), "Monthly Visit", "Monthly Visit 8");
-    // monthlyEncounterDueDate = addEncounter(_.addDays(monthlyEncounterDueDate, 30), "Monthly Visit", "Monthly Visit 9");
-    // monthlyEncounterDueDate = addEncounter(_.addDays(monthlyEncounterDueDate, 30), "Monthly Visit", "Monthly Visit 10");
-    // monthlyEncounterDueDate = addEncounter(_.addDays(monthlyEncounterDueDate, 30), "Monthly Visit", "Monthly Visit 11");
-    // addEncounter(_.addDays(monthlyEncounterDueDate, 30), "Monthly Visit", "Monthly Visit 12");
-    //
-    // //Quarterly Encounter
-    // let quarterlyEncounterDueDate = addEncounter(today, "Quarterly Visit", "Quarterly Visit 1");
-    // quarterlyEncounterDueDate = addEncounter(_.addDays(quarterlyEncounterDueDate, 90), "Quarterly Visit", "Quarterly Visit 2");
-    // quarterlyEncounterDueDate = addEncounter(_.addDays(quarterlyEncounterDueDate, 90), "Quarterly Visit", "Quarterly Visit 3");
-    // quarterlyEncounterDueDate = addEncounter(_.addDays(quarterlyEncounterDueDate, 90), "Quarterly Visit", "Quarterly Visit 4");
-    //
-    // //Half Yearly Encounter
-    // let halfYearlyEncounterDueDate = addEncounter(today, "Half Yearly Visit", "Half Yearly Visit 1");
-    // halfYearlyEncounterDueDate = addEncounter(_.addDays(halfYearlyEncounterDueDate, 180), "Half Yearly Visit", "Half Yearly Visit 2");
-
-    // addEncounter(today, "Annual Visit", "Annual Visit");
-
-    return encounters;
+    return [];
 };
 
 export {getNextScheduledVisits};
