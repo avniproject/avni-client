d1566a37e140994719922644f1bb70e957fac328
mihirk <mihir.khatwani@gmail.com>
2017-12-26 11:22:16 +0530

    Avoiding rescheduling of duplicate drop out home visits

--- packages/openchs-health-modules/health_modules/adolescent/adolescentProgramEnrolmentDecision.js
+++ packages/openchs-health-modules/health_modules/adolescent/adolescentProgramEnrolmentDecision.js
@@ -7,7 +7,7 @@ import VisitScheduleBuilder from "../rules/VisitScheduleBuilder";
 
 const getDecisions = (programEnrolment, context, today) => {
     if (context.usage === 'Exit')
-    return {enrolmentDecisions: [], encounterDecisions: []};
+        return {enrolmentDecisions: [], encounterDecisions: []};
 
     return vulnerabilityEnrolmentDecisions(programEnrolment);
 };
@@ -25,15 +25,18 @@ const getNextScheduledVisits = function (programEnrolment, today, currentEncount
         earliestDate: programEnrolment.enrolmentDateTime,
         maxDate: C.addDays(C.copyDate(programEnrolment.enrolmentDateTime), 10)
     }).whenItem(programEnrolment.getEncounters().length).equals(0);
+    const existingUnfinishedDropoutHomeVisit = programEnrolment.encounters
+        .filter(encounter => encounter.encounterType.name === "Dropout Home Visit"
+            && _.isNil(encounter.encounterDateTime));
     scheduleBuilder.add({
             name: "Dropout Home Visit",
             encounterType: "Dropout Home Visit",
             earliestDate: programEnrolment.enrolmentDateTime,
             maxDate: C.addDays(C.copyDate(programEnrolment.enrolmentDateTime), 15)
         }
-    ).when.valueInEnrolment("School going").containsAnswerConceptName("Dropped Out");
+    ).when.valueInEnrolment("School going").containsAnswerConceptName("Dropped Out")
+        .and.whenItem(existingUnfinishedDropoutHomeVisit.length).equals(0);
     let all = scheduleBuilder.getAll();
-    console.log(all);
     return all;
 };
 
