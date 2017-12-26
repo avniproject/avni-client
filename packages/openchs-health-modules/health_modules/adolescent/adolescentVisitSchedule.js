f90f5c7ce81a245da7d6a1654e256d42b4b96c52
mihirk <mihir.khatwani@gmail.com>
2017-12-26 06:33:25 +0530

    Adding visit schedule for follow up visits

--- packages/openchs-health-modules/health_modules/adolescent/adolescentVisitSchedule.js
+++ packages/openchs-health-modules/health_modules/adolescent/adolescentVisitSchedule.js
@@ -1,5 +1,6 @@
 import C from '../common';
 import VisitScheduleBuilder from "../rules/VisitScheduleBuilder";
+import moment from 'moment';
 
 const encounterSchedule = {
     "Monthly Visit": {earliest: 30, max: 40},
@@ -20,6 +21,26 @@ const getNextScheduledVisits = function (programEncounter) {
             maxDate: C.addDays(C.copyDate(new Date()), 15)
         }
     ).when.valueInEncounter("School going").containsAnswerConceptName("Dropped Out");
+
+    scheduleBuilder.add({
+            name: "School Dropout Followup",
+            encounterType: "Dropout Followup Visit",
+            earliestDate: C.addDays(new Date(), 7),
+            maxDate: C.addDays(C.copyDate(new Date()), 10)
+        }
+    ).when.valueInEncounter("Have you started going to school once again").containsAnswerConceptName("No")
+        .and.whenItem(programEncounter.programEnrolment.encounters
+        .filter((encounter) => encounter.encounterType.name === "Dropout Followup Visit").length).lessThanOrEqualTo(5);
+
+    scheduleBuilder.add({
+            name: "School Dropout Followup",
+            encounterType: "Dropout Followup Visit",
+            earliestDate: moment().month(3).date(1).hour(0).minute(0).second(0).toDate(),
+            maxDate: C.addDays(C.copyDate(moment().month(3).date(1).hour(0).minute(0).second(0).toDate()), 10)
+        }
+    ).when.valueInEncounter("Have you started going to school once again")
+        .containsAnswerConceptName("Yes, but could not attend");
+
     return scheduleBuilder.getAll();
 };
 
