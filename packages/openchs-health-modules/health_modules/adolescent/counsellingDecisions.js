a94331d5c7f3f7fd152c3e393597f66dbde934e8
mihirk <mihir.khatwani@gmail.com>
2018-01-08 17:12:42 +0530

    Reformatting counselling decision

--- packages/openchs-health-modules/health_modules/adolescent/counsellingDecisions.js
+++ packages/openchs-health-modules/health_modules/adolescent/counsellingDecisions.js
@@ -3,16 +3,18 @@ import C from '../common';
 import _ from 'lodash'
 
 const encounterDecisions = (vulnerabilityEncounterDecisions, programEncounter) => {
-    const complicationsBuilder = new ComplicationsBuilder({complicationsConcept: 'Adolescent Counselling Advice',
-        programEncounter: programEncounter});
+    const complicationsBuilder = new ComplicationsBuilder({
+        complicationsConcept: 'Adolescent Counselling Advice',
+        programEncounter: programEncounter
+    });
 
 
-    let reasonsForSchoolDropoutVulnerability = C.findValue(vulnerabilityEncounterDecisions.encounterDecisions, "Reason for School Dropout Vulnerability");
-    if (!_.isEmpty(reasonsForSchoolDropoutVulnerability)) {
-        if (reasonsForSchoolDropoutVulnerability.includes('Severe Anemia'))
-            complicationsBuilder.addComplication("Severe Anemia Counselling");
-        if (reasonsForSchoolDropoutVulnerability.includes('No Parents / Single Parent'))
-            complicationsBuilder.addComplication("No Parents / Single Parent Counselling");
+    let reasonsForSchoolDropoutVulnerability = C.findValue(vulnerabilityEncounterDecisions.encounterDecisions,
+        "Reason for School Dropout Vulnerability");
+
+    if (!_.isEmpty(reasonsForSchoolDropoutVulnerability)
+        && reasonsForSchoolDropoutVulnerability.includes('Severe Anemia')) {
+        complicationsBuilder.addComplication("Severe Anemia Counselling");
     }
 
     vulnerabilityEncounterDecisions.encounterDecisions.push(complicationsBuilder.getComplications());
