7d93c5e9bc6bbe22cd616f2febb5ae0544baa62c
mihirk <mihir.khatwani@gmail.com>
2018-01-15 10:20:15 +0530

    Removing useless test

--- packages/openchs-health-modules/test/IndividualRegistrationTest.js
+++
@@ -1,9 +0,0 @@
-const assert = require('chai').assert;
-
-describe('IndividualRegistrationTest', () => {
-    it('wiring', () => {
-        const imports = require('../health_modules/individualRegistrationDecision');
-        assert.isUndefined(imports.validate);
-        assert.isUndefined(imports.numberOfFormElementGroups);
-    });
-});
