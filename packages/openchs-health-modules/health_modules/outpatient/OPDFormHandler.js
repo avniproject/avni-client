7b3462b85ce01f80dcd60aecaa237fc9ebb71717
Charles John <charlesj@gmail.com>
2018-03-16 16:12:30 +0530

    #411 - Added "Referral" question to OPD form

--- packages/openchs-health-modules/health_modules/outpatient/OPDFormHandler.js
+++ packages/openchs-health-modules/health_modules/outpatient/OPDFormHandler.js
@@ -8,6 +8,13 @@ export default class {
         return statusBuilder.build();
     }
 
+    reasonForReferral(encounter, formElement) {
+        let statusBuilder = this._getStatusBuilder(encounter, formElement);
+        statusBuilder.show().when
+            .valueInEncounter("Is outpatient referred to higher health center?").containsAnswerConceptName("Yes");
+        return statusBuilder.build();
+    }
+
     _getStatusBuilder(encounter, formElement) {
         return new FormElementStatusBuilder({
             programEncounter: encounter,
