6b9d6eaa2b17b59d55f3d85f80817b8e5647bfdd
Vinay Venu <ijkvinay@gmail.com>
2018-04-16 10:05:26 +0530

    Remove ANC schedule tests

--- packages/openchs-health-modules/test/MotherVisitScheduleTest.js
+++ packages/openchs-health-modules/test/MotherVisitScheduleTest.js
@@ -4,34 +4,17 @@ const ProgramEnrolment = require("./Entities").ProgramEnrolment;
 const ProgramEncounter = require("./Entities").ProgramEncounter;
 
 describe('Create ANC/PNC Visit Schedule', function () {
-    const matchDate = function (date1, date2) {
-        return date1.getFullYear() === date2.getFullYear() && date1.getMonth() === date2.getMonth() && date1.getDate() === date2.getDate();
-    };
-
     var enrolment;
 
-    //[new ProgramEncounter('ANC', new Date(2017, 1, 3), 'ANC 1')]
     beforeEach(function() {
         enrolment = new ProgramEnrolment('Mother', []);
         enrolment.setObservation('Last menstrual period', new Date(2017, 0, 3));
     });
 
-    it ("Creates all ANC visits for a fresh enrolment", function() {
+    it ("No ANC Visits are scheduled", function() {
         var scheduledVisits = motherVisitSchedule.getNextScheduledVisits(enrolment);
-        expect(scheduledVisits).to.be.an('array');
-        expect(scheduledVisits).to.have.lengthOf(5);
-    });
-
-    it ("Does not recreate visits if they are already present", function() {
-        enrolment.encounters.push({ name: 'ANC 1', encounterType: {name: 'ANC'}, earliestDate: new Date(), maxDate: new Date()});
-        var scheduledVisits = motherVisitSchedule.getNextScheduledVisits(enrolment);
-        expect(scheduledVisits).to.have.lengthOf(4);
-    });
-
-    it ("Does not recreate visits if they are already present", function() {
-        enrolment.encounters.push({ name: 'Delivery', encounterType: {name: 'Delivery'}, earliestDate: new Date(), maxDate: new Date(), encounterDateTime: new Date()});
-        var scheduledVisits = motherVisitSchedule.getNextScheduledVisits(enrolment);
-        expect(scheduledVisits).to.have.lengthOf(8);
+        expect(scheduledVisits).to.be.an('array').with.lengthOf(0);
+        expect(scheduledVisits).to.have.lengthOf(0);
     });
 });
 
