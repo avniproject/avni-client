826fb5fa8c26fd7b1c44d6132ebc0ca7d8f0191c
A.N.Sidtharthan <sidthunagarajan@gmail.com>
2018-07-04 19:00:52 +0530

    Add `Ability to access core modules functions from Impl. rules`

--- packages/openchs-android/src/service/RuleService.js
+++ packages/openchs-android/src/service/RuleService.js
@@ -4,6 +4,7 @@ import Service from '../framework/bean/Service';
 import Rule from "../../../openchs-models/src/Rule";
 import RuleDependency from "../../../openchs-models/src/RuleDependency";
 import General from "../utility/General";
+import { C } from "openchs-health-modules";
 
 @Service("ruleService")
 class RuleService extends BaseService {
@@ -18,7 +19,11 @@ class RuleService extends BaseService {
         /**********/
         /*variables used inside the eval*/
         let rulesConfig = undefined;
-        let CHSLogger = console.log;
+        /*keeping it long to avoid name conflicts*/
+        let ruleServiceLibraryInterfaceForSharingModules = {
+            log: console.log,
+            calculations: C
+        };
         eval(RuleDependency.getCode(ruleDependency));
         /**********/
         this.allRules = {...rulesConfig};
@@ -30,6 +35,9 @@ class RuleService extends BaseService {
         const rules = this.db.objects(Rule.schema.name)
             .filtered(`voided = false and form.uuid=$0 and type=$1`, form.uuid, type)
             .map(_.identity);
+        if(rules.length == 0) {
+            General.logDebug("RuleService", `No Rules of Type ${type} for Form - ${form.name} ${form.uuid} exists`);
+        }
         return _.defaults(rules, [])
             .filter(ar => _.isFunction(this.allRules[ar.fnName]) && _.isFunction(this.allRules[ar.fnName].exec))
             .map(ar => ({...ar, fn: this.allRules[ar.fnName]}));
