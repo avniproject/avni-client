36f70a2ae0463fbd91e495bb10dc92ca8b7f9cbd
A.N.Sidtharthan <sidthunagarajan@gmail.com>
2018-07-24 19:09:49 +0530

    Move pregnancy program weight gain logic to `mother/calculations` file, so that Implementation rules can invoke them through `lib` interface
    Also add isBelowNormalWeightGain for showing Diet advice.

--- packages/openchs-health-modules/health_modules/mother/utils.js
+++ packages/openchs-health-modules/health_modules/mother/utils.js
@@ -1,45 +1,7 @@
 import { FormElementsStatusHelper } from "rules-config/rules";
 import _ from "lodash";
 import C from '../common';
-const { weeksBetween } = FormElementsStatusHelper;
-
-const TRIMESTER_MAPPING = new Map([
-    [1, { from: 0, to: 12 }],
-    [2, { from: 13, to: 28 }],
-    [3, { from: 29, to: 40 }]
-]);
-
-export let gestationalAge = (enrolment, toDate = new Date()) => weeksBetween(toDate,
-    enrolment.getObservationValue("Last menstrual period"));
-
-export let currentTrimester = (enrolment, toDate = new Date()) => [...TRIMESTER_MAPPING.keys()]
-    .find((trimester) =>
-        gestationalAge(enrolment, toDate) <= TRIMESTER_MAPPING.get(trimester).to &&
-        gestationalAge(enrolment, toDate) >= TRIMESTER_MAPPING.get(trimester).from);
-
-const get1stEncounterAfter1stTrimester = (enrolment, encounterType) => {
-    return _(enrolment.encounters)
-        .filter((en) => _.get(en, "encounterType.name") === encounterType)
-        .filter((en) => currentTrimester(enrolment, en.encounterDateTime) > 1)
-        .sortBy('encounterDateTime')
-        .first();
-}
-
-export let isNormalWeightGain = (enrolment, currentEncounter) => {
-    const baseEncounter = get1stEncounterAfter1stTrimester(enrolment, _.get(currentEncounter, "encounterType.name"));
-    const baseWeight = baseEncounter && baseEncounter.getObservationValue('Weight');
-    const currentWeight = currentEncounter && currentEncounter.getObservationValue("Weight");
-    if (_.some([baseEncounter, currentEncounter, baseWeight, currentWeight], _.isNil)) {
-        return true;
-    }
-
-    const noOfWeeksBetween = weeksBetween(currentEncounter.encounterDateTime, baseEncounter.encounterDateTime);
-    const minWeightGain = noOfWeeksBetween * 1.7 / 4;
-    const maxWeightGain = noOfWeeksBetween * 2 / 4;
-    const lowerBound = baseWeight + minWeightGain;
-    const upperBound = baseWeight + maxWeightGain;
-    return lowerBound <= currentWeight && currentWeight <= upperBound;
-};
+export { isNormalWeightGain, currentTrimester, gestationalAge } from './calculations';
 
 let cmIncrease = (enrolment, encounter, multiplicationFactor, conceptName, toDate = new Date()) => {
     const lastEncounter = enrolment.findLastEncounterOfType(encounter, [_.get(encounter, "encounterType.name")]);
