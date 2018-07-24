74e7cb1804db07eba77c78951326890a77b3d8dc
Charles John <charlesj@gmail.com>
2018-07-24 15:15:38 +0530

    #571 - On error, show message to user before restart

--- packages/openchs-android/src/App.js
+++ packages/openchs-android/src/App.js
@@ -1,4 +1,4 @@
-import {Dimensions} from "react-native";
+import {Alert, Dimensions, NativeModules, View, Clipboard} from "react-native";
 import React, {Component} from 'react';
 import PathRegistry from './framework/routing/PathRegistry';
 import BeanRegistry from './framework/bean/BeanRegistry';
@@ -8,13 +8,17 @@ import './views';
 import AppStore from './store/AppStore';
 import EntitySyncStatusService from "./service/EntitySyncStatusService";
 import ErrorHandler from './utility/ErrorHandler';
+import _ from "lodash";
 
+const {Restart} = NativeModules;
 let routes, beans, reduxStore, db = undefined;
 
 export default class App extends Component {
     constructor(props, context) {
         super(props, context);
-        ErrorHandler.set();
+        ErrorHandler.set((error, stacktrace) => {
+            this.setState({error, stacktrace});
+        });
         if (db === undefined) {
             db = new Realm(Schema);
             beans = BeanRegistry.init(db, this);
@@ -24,12 +28,13 @@ export default class App extends Component {
             entitySyncStatusService.setup(EntityMetaData.model());
         }
         this.getBean = this.getBean.bind(this);
+        this.state = {error: null}
     }
 
     static childContextTypes = {
         getService: React.PropTypes.func.isRequired,
         getDB: React.PropTypes.func.isRequired,
-        getStore: React.PropTypes.func.isRequired
+        getStore: React.PropTypes.func.isRequired,
     };
 
     getChildContext = () => ({
@@ -37,14 +42,33 @@ export default class App extends Component {
         getService: (serviceName) => {
             return beans.get(serviceName)
         },
-        getStore: () => reduxStore
+        getStore: () => reduxStore,
     });
+
+    renderError() {
+        Alert.alert("App will restart now", this.state.error.message,
+            [
+                {text: "Copy error and Restart",
+                    onPress: () => {
+                        Clipboard.setString(`${this.state.error.message}\nStacktrace:\n${this.state.stacktrace}`);
+                        Restart.restart()
+                    }
+                }
+            ],
+            {cancelable: false}
+        );
+        return <View/>;
+    }
 
     getBean(name) {
         return beans.get(name);
     }
 
     render() {
-        return routes;
+        if (!_.isNil(this.state.error)) {
+            return this.renderError();
+        } else {
+            return routes;
+        }
     }
 }
