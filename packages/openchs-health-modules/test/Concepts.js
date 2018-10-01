e49f416954b9328f5256cca3e44a66733269baca
Vivek Singh <petmongrels@gmail.com>
2018-10-01 13:08:13 +0530

    fixed tests dependent on defunct ncd module

--- packages/openchs-health-modules/test/Concepts.js
+++ packages/openchs-health-modules/test/Concepts.js
@@ -8,20 +8,18 @@ const OPDConcepts = require('../health_modules/outpatient/metadata/concepts.json
 const ChildProgramEncounterForm = require('../health_modules/child/metadata/childDefaultProgramEncounterForm.json');
 const ChildProgramEnrolmentForm = require('../health_modules/child/metadata/childProgramEnrolmentForm.json');
 const ChildProgramExitForm = require('../health_modules/child/metadata/childProgramExitForm.json');
-const DiabetesProgramEncounterForm = require('../health_modules/diabetes/metadata/diabetesProgramEncounterForm.json');
 const MotherAbortionForm = require('../health_modules/mother/metadata/motherAbortionForm.json');
 const MotherANCForm = require('../health_modules/mother/metadata/motherANCForm.json');
 const MotherDeliveryForm = require('../health_modules/mother/metadata/motherDeliveryForm.json');
 const MotherPNCForm = require('../health_modules/mother/metadata/motherPNCForm.json');
 const MotherProgramEnrolmentForm = require('../health_modules/mother/metadata/motherProgramEnrolmentForm.json');
 const MotherProgramExitForm = require('../health_modules/mother/metadata/motherProgramExitForm.json');
-const ScreeningEncounterForm = require('../health_modules/ncd/metadata/screeningEncounterForm.json');
 const OPDEncounterForm = require('../health_modules/outpatient/metadata/encounterForm.json');
 import {Concept} from "openchs-models";
 
 
 const IMPORTED_CONCEPTS = _.flatten([CommonConcepts, ChildConcepts, MotherConcepts, OPDConcepts]);
-const FORM_CONCEPTS = _.flatten(_.flatten([ChildProgramEncounterForm, ChildProgramEnrolmentForm, ChildProgramExitForm, DiabetesProgramEncounterForm, MotherAbortionForm, MotherANCForm, MotherDeliveryForm, MotherPNCForm, MotherProgramEnrolmentForm, MotherProgramExitForm, ScreeningEncounterForm, OPDEncounterForm]
+const FORM_CONCEPTS = _.flatten(_.flatten([ChildProgramEncounterForm, ChildProgramEnrolmentForm, ChildProgramExitForm, MotherAbortionForm, MotherANCForm, MotherDeliveryForm, MotherPNCForm, MotherProgramEnrolmentForm, MotherProgramExitForm, OPDEncounterForm]
     .map((formDef) => formDef.formElementGroups.map((fegs) => fegs.formElements))));
 
 let concepts = _.mapValues(_.groupBy(IMPORTED_CONCEPTS.concat(FORM_CONCEPTS), 'name'), (val) => val[0]);
