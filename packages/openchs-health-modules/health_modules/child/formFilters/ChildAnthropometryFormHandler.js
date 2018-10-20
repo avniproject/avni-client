7a053170693381a748a1700033ec7d84a79ade54
Vivek Singh <petmongrels@gmail.com>
2018-10-20 09:09:54 +0530

    made it consistent with how it is defined elsehwre in core

--- packages/openchs-health-modules/health_modules/child/formFilters/ChildAnthropometryFormHandler.js
+++ packages/openchs-health-modules/health_modules/child/formFilters/ChildAnthropometryFormHandler.js
@@ -3,13 +3,20 @@ import {FormElementStatusBuilder} from "rules-config/rules";
 class ChildAnthropometryFormHandler {
 
     height(programEncounter, formElement) {
-        const statusBuilder = new FormElementStatusBuilder({programEncounter, formElement});
+        const statusBuilder = new FormElementStatusBuilder({
+            programEncounter: programEncounter,
+            formElement: formElement
+        });
         statusBuilder.show().when.valueInEncounter("Skip capturing height").is.notDefined;
         return statusBuilder.build();
     }
 
     reasonForSkippingHeightCapture(programEncounter, formElement) {
-        const statusBuilder = new FormElementStatusBuilder({programEncounter, formElement});
+        console.log(JSON.stringify(programEncounter.observations));
+        const statusBuilder = new FormElementStatusBuilder({
+            programEncounter: programEncounter,
+            formElement: formElement
+        });
         statusBuilder.show().when.valueInEncounter("Skip capturing height").is.yes;
         return statusBuilder.build();
     }
