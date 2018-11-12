avniproject/avni-client

commit 66b84205d15cd382f228bf3a92762683adebf943

[openchs/#945] Setup bug snag for staging

packages/openchs-android/src/store/AppStore.js
@@ -6,9 +6,7 @@ import ErrorHandler from "../utility/ErrorHandler";
 class AppStore {
     static create(beans, errorCallback) {
         const combinedReducers = this.createCombinedReducer(beans);
-        return __DEV__ ?
-            createStore(combinedReducers) :
-            createStore(combinedReducers, applyMiddleware(AppStore.middlewareFactory(AppStore.errorHandler, errorCallback)));
+        return createStore(combinedReducers, applyMiddleware(AppStore.middlewareFactory(AppStore.errorHandler, errorCallback)));
     }
 
     static errorHandler(error, errorCallback, getState, lastAction, dispatch) {

packages/openchs-android/src/utility/ErrorHandler.js
@@ -3,11 +3,9 @@ import bugsnag from './bugsnag';
 
 export default class ErrorHandler {
     static set(errorCallback) {
-        if (!__DEV__) {
             ErrorUtils.setGlobalHandler((error, isFatal) => {
                 ErrorHandler.postError(error, isFatal, errorCallback);
             });
-        }
     }
 
     static postError(error, isFatal, errorCallback) {

packages/openchs-android/.env.live
@@ -1,1 +1,2 @@
 SERVER_URL=https://server.openchs.org
+ENV=prod

packages/openchs-android/.env.staging
@@ -1,2 +1,3 @@
 SERVER_URL=https://staging.openchs.org
 KEY_STORE_PREFIX=staging
+ENV=staging

packages/openchs-android/src/service/SettingsService.js
@@ -4,7 +4,7 @@ import InitialSettings from '../../config/initialSettings.json';
 import AvailableLocales from '../../config/AvailableLocales.json';
 import General from "../utility/General";
 import {Settings, LocaleMapping} from "openchs-models";
-import Config from 'react-native-config'
+import Config from 'react-native-config';
 import _ from 'lodash';
 import ModelGeneral from "../../../openchs-models/src/utility/General";
 

packages/openchs-android/src/utility/bugsnag.js
@@ -1,12 +1,16 @@
 //-------------------------------------------------------------------------------------------------
-// Create a singleton instance of the bugsnag client so we don't have to duplicate our configuration
-// anywhere.
+// Purpose of this file is to create a singleton instance of the bugsnag client 
+// so we don't have to duplicate our configuration anywhere.
 //-------------------------------------------------------------------------------------------------
-// https://docs.bugsnag.com/platforms/react-native/#basic-configuration
+
 import { Client, Configuration } from 'bugsnag-react-native';
+import Config from 'react-native-config';
+
 const configuration = new Configuration();
 configuration.autoNotify = false;
+configuration.releaseStage = Config.ENV;
+configuration.notifyReleaseStages = ['staging', 'prod'];
 console.log("Creating new instance of Bugsnag");
 const client = new Client(configuration);
-//-------------------------------------------------------------------------------------------------
+
 export default client;

commit 4caf4974ad4f61638efbfd3586c91ff5e9078113

Fixing local error handling to show stack trace instead of "Copy Error and Restart". This was broken by https://github.com/OpenCHS/openchs-client/commit/66b84205d15cd382f228bf3a92762683adebf943

packages/openchs-android/src/store/AppStore.js
@@ -2,11 +2,14 @@ import {createStore, combineReducers, applyMiddleware} from 'redux';
 import Reducers from '../reducer';
 import General from "../utility/General";
 import ErrorHandler from "../utility/ErrorHandler";
+import Config from 'react-native-config';
 
 class AppStore {
     static create(beans, errorCallback) {
         const combinedReducers = this.createCombinedReducer(beans);
-        return createStore(combinedReducers, applyMiddleware(AppStore.middlewareFactory(AppStore.errorHandler, errorCallback)));
+        return Config.ENV === 'dev' ?
+            createStore(combinedReducers) :
+            createStore(combinedReducers, applyMiddleware(AppStore.middlewareFactory(AppStore.errorHandler, errorCallback)));
     }
 
     static errorHandler(error, errorCallback, getState, lastAction, dispatch) {

packages/openchs-android/src/utility/ErrorHandler.js
@@ -1,11 +1,14 @@
 import StackTrace from 'stacktrace-js';
 import bugsnag from './bugsnag';
+import Config from 'react-native-config';
 
 export default class ErrorHandler {
     static set(errorCallback) {
+        if (Config.ENV !== 'dev') {
             ErrorUtils.setGlobalHandler((error, isFatal) => {
                 ErrorHandler.postError(error, isFatal, errorCallback);
             });
+        }
     }
 
     static postError(error, isFatal, errorCallback) {