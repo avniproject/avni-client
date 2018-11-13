b413ceca3bde7d775cb2b9ef098a0ee37d232300
Hiren Thacker <hithacker@gmail.com>
2018-11-13 17:07:14 +0530

    [openchs/#975] Date fields showing time value
    This commit take cares of two things:
    1. Do not show time information for date concept.
    2. Do not store time information for date concepts.

--- packages/openchs-models/test/PrimitiveValueTest.js
+++ packages/openchs-models/test/PrimitiveValueTest.js
@@ -1,16 +1,52 @@
 import {assert} from 'chai';
-import PrimitiveValue from "../src/observation/PrimitiveValue";
-import Concept from "../src/Concept";
+import PrimitiveValue from '../src/observation/PrimitiveValue';
+import Concept from '../src/Concept';
+import General from '../src/utility/General';
 
-describe('PrimitiveValueTest', () => {
-    it('Primitive Value With Time', () => {
-        const date = new Date(2018, 1, 1, 1, 1, 0, 0);
-        const primitiveValue = new PrimitiveValue(date, Concept.dataType.DateTime);
-        assert.equal("01-Feb-2018 01:01", primitiveValue.asDisplayDate())
+describe('getValue', () => {
+    describe('when concept datatype is DateTime', () => {
+        it('returns Date with time data', () => {
+            const date = new Date(2018, 1, 1, 1, 1, 0, 0);
+            const primitiveValue = new PrimitiveValue(date, Concept.dataType.DateTime);
+            assert(!General.hoursAndMinutesOfDateAreZero(primitiveValue.getValue()), "time data should be there");
+        });
     });
-    it('Primitive Value Without Time', () => {
-        const date = new Date(2018, 1, 1);
-        const primitiveValue = new PrimitiveValue(date, Concept.dataType.DateTime);
-        assert.equal("01-Feb-2018", primitiveValue.asDisplayDate())
+    describe('when concept datatype is Date', () => {
+        it('returns Date without time data', () => {
+            const date = new Date(2018, 1, 1, 1, 1, 0, 0);
+            const primitiveValue = new PrimitiveValue(date, Concept.dataType.Date);
+            assert(General.hoursAndMinutesOfDateAreZero(primitiveValue.getValue()), "time data should not be there");
+        });
+    });
+
+});
+
+describe('asDisplayDate', () => {
+    describe('when concept datatype is DateTime', () => {
+        it('returns time info when value date have time data', () => {
+            const date = new Date(2018, 1, 1, 1, 1, 0, 0);
+            const primitiveValue = new PrimitiveValue(date, Concept.dataType.DateTime);
+            assert.equal("01-Feb-2018 01:01", primitiveValue.asDisplayDate());
+        });
+
+        it('does not return time info when value date do not have time data', () => {
+            const date = new Date(2018, 1, 1);
+            const primitiveValue = new PrimitiveValue(date, Concept.dataType.DateTime);
+            assert.equal("01-Feb-2018", primitiveValue.asDisplayDate());
+        });
+    });
+
+    describe('when concept datatype is Date', () => {
+        it('does not return time info when value date have time data', () => {
+            const date = new Date(2018, 1, 1, 1, 1, 0, 0);
+            const primitiveValue = new PrimitiveValue(date, Concept.dataType.Date);
+            assert.equal("01-Feb-2018", primitiveValue.asDisplayDate());
+        });
+
+        it('does not return time info when value date do not have time data', () => {
+            const date = new Date(2018, 1, 1);
+            const primitiveValue = new PrimitiveValue(date, Concept.dataType.Date);
+            assert.equal("01-Feb-2018", primitiveValue.asDisplayDate());
+        });
     });
 });
