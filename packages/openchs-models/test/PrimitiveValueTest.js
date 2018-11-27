53f5649de30631ab2c55f09c2d61d673c939a701
Sidtharthan A N <sidthunagarajan@gmail.com>
2018-11-27 12:57:50 +0530

    Fix PrimitiveValueTest. Use UTC Timezone for assertions.

--- packages/openchs-models/test/PrimitiveValueTest.js
+++ packages/openchs-models/test/PrimitiveValueTest.js
@@ -1,4 +1,4 @@
-import {assert} from 'chai';
+import {assert, expect} from 'chai';
 import PrimitiveValue from '../src/observation/PrimitiveValue';
 import Concept from '../src/Concept';
 import General from '../src/utility/General';
@@ -6,16 +6,17 @@ import General from '../src/utility/General';
 describe('getValue', () => {
     describe('when concept datatype is DateTime', () => {
         it('returns Date with time data', () => {
-            const date = new Date(2018, 1, 1, 1, 1, 0, 0);
+            const date = '2018-11-27T12:34:56.000Z';
             const primitiveValue = new PrimitiveValue(date, Concept.dataType.DateTime);
-            assert(!General.hoursAndMinutesOfDateAreZero(primitiveValue.getValue()), "time data should be there");
+            expect(primitiveValue.getValue().toISOString()).to.equal(date);
         });
     });
+
     describe('when concept datatype is Date', () => {
         it('returns Date without time data', () => {
-            const date = new Date(2018, 1, 1, 1, 1, 0, 0);
+            const date = '2018-11-27T12:34:56.000Z';
             const primitiveValue = new PrimitiveValue(date, Concept.dataType.Date);
-            assert(General.hoursAndMinutesOfDateAreZero(primitiveValue.getValue()), "time data should not be there");
+            expect(primitiveValue.getValue().toISOString()).to.equal('2018-11-27T00:00:00.000Z');
         });
     });
 
@@ -38,13 +39,13 @@ describe('asDisplayDate', () => {
 
     describe('when concept datatype is Date', () => {
         it('does not return time info when value date have time data', () => {
-            const date = new Date(2018, 1, 1, 1, 1, 0, 0);
+            const date = '2018-02-01T12:34:56.000Z';
             const primitiveValue = new PrimitiveValue(date, Concept.dataType.Date);
             assert.equal("01-Feb-2018", primitiveValue.asDisplayDate());
         });
 
         it('does not return time info when value date do not have time data', () => {
-            const date = new Date(2018, 1, 1);
+            const date = '2018-02-01T00:00:00.000Z';
             const primitiveValue = new PrimitiveValue(date, Concept.dataType.Date);
             assert.equal("01-Feb-2018", primitiveValue.asDisplayDate());
         });
