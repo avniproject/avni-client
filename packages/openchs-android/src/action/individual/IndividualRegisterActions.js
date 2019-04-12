avniproject/avni-client

commit 37074f5f010b0f0ce384836bfce78dd964e4a18c

1304 | Ability to sync identifiers and save them as part of registration

packages/openchs-android/src/action/individual/IndividualRegisterActions.js
@@ -1,10 +1,11 @@
 import IndividualService from "../../service/IndividualService";
 import ObservationsHolderActions from "../common/ObservationsHolderActions";
 import EntityService from "../../service/EntityService";
-import {Gender, Form, Individual, Point, SubjectType} from "openchs-models";
+import {Gender, Form, Individual, Point, SubjectType, ObservationsHolder} from "openchs-models";
 import IndividualRegistrationState from "../../state/IndividualRegistrationState";
 import _ from 'lodash';
 import GeolocationActions from "../common/GeolocationActions";
+import IdentifierAssignmentService from "../../service/IdentifierAssignmentService";
 
 export class IndividualRegisterActions {
     static getInitialState(context) {
@@ -22,6 +23,10 @@ export class IndividualRegisterActions {
         if (_.isEmpty(individual.subjectType.name)) {
             individual.subjectType = state.individualSubjectType;
         }
+
+        //Populate identifiers much before form elements are hidden or sent to rules.
+        //This will enable the value to be used in rules
+        context.get(IdentifierAssignmentService).populateIdentifiers(form, new ObservationsHolder(individual.observations));
 
         const newState = IndividualRegistrationState.createLoadState(state.form, state.genders, individual);
         IndividualRegisterActions.setAgeState(newState);

packages/openchs-android/src/action/program/ProgramEnrolmentActions.js
@@ -7,8 +7,9 @@ import _ from "lodash";
 import {StaticFormElementGroup, ProgramEnrolment} from "openchs-models";
 import ConceptService from "../../service/ConceptService";
 import RuleEvaluationService from "../../service/RuleEvaluationService";
-import {Point} from "openchs-models";
+import {Point, ObservationsHolder} from "openchs-models";
 import GeolocationActions from "../common/GeolocationActions";
+import IdentifierAssignmentService from "../../service/IdentifierAssignmentService";
 
 export class ProgramEnrolmentActions {
     static getInitialState(context) {
@@ -25,6 +26,11 @@ export class ProgramEnrolmentActions {
             const isNewEnrolment = _.isNil(action.enrolment.uuid)
                 ? true
                 : _.isNil(context.get(ProgramEnrolmentService).findByUUID(action.enrolment.uuid));
+
+            //Populate identifiers much before form elements are hidden or sent to rules.
+            //This will enable the value to be used in rules
+            context.get(IdentifierAssignmentService).populateIdentifiers(form, new ObservationsHolder(action.enrolment.observations));
+
             const formElementGroup = _.isNil(form) ? new StaticFormElementGroup(form) : form.firstFormElementGroup;
             const numberOfPages = _.isNil(form) ? 1 : form.numberOfPages;
             let formElementStatuses = context

packages/openchs-android/src/action/subject/SubjectRegisterActions.js
@@ -2,10 +2,11 @@ import IndividualService from "../../service/IndividualService";
 import ObservationsHolderActions from "../common/ObservationsHolderActions";
 import GeolocationActions from "../common/GeolocationActions";
 import EntityService from "../../service/EntityService";
-import {Form, Individual, SubjectType, Point} from "openchs-models";
+import {Form, Individual, SubjectType, Point, ObservationsHolder} from "openchs-models";
 import SubjectRegistrationState from '../../state/SubjectRegistrationState';
 import _ from 'lodash';
 import RuleEvaluationService from "../../service/RuleEvaluationService";
+import IdentifierAssignmentService from "../../service/IdentifierAssignmentService";
 
 export class SubjectRegisterActions {
     static getInitialState(context) {
@@ -26,6 +27,11 @@ export class SubjectRegisterActions {
         if (_.isEmpty(subject.subjectType.name)) {
             subject.subjectType = state.subjectType;
         }
+
+        //Populate identifiers much before form elements are hidden or sent to rules.
+        //This will enable the value to be used in rules
+        let observationsHolder = new ObservationsHolder(subject.observations);
+        context.get(IdentifierAssignmentService).populateIdentifiers(state.form, observationsHolder);
 
         let firstGroupWithAtLeastOneVisibleElement = _.find(_.sortBy(state.form.nonVoidedFormElementGroups(), [function (o) {
             return o.displayOrder
@@ -57,7 +63,6 @@ export class SubjectRegisterActions {
     static enterSubjectAddressLevel(state, action) {
         const newState = state.clone();
         newState.subject.lowestAddressLevel = action.value;
-        console.log('SubjectRegisterActions.enterSubjectAddressLevel',newState.subject.lowestAddressLevel);
         newState.handleValidationResult(newState.subject.validateAddress());
         return newState;
     }

packages/openchs-android/src/service/BaseService.js
@@ -57,7 +57,7 @@ class BaseService {
         return this.getReturnValue(allEntities);
     }
 
-    findByKey(keyName, value, schemaName) {
+    findByKey(keyName, value, schemaName = this.getSchema()) {
         const entities = this.findAllByKey(keyName, value, schemaName);
         return this.getReturnValue(entities);
     }
@@ -87,6 +87,7 @@ class BaseService {
     }
 
     createEntities(schema, entities) {
+        console.log('saving for schema', schema);
         return entities.map((entity) => () => {
             this.db.create(schema, entity, true)
         });

packages/openchs-android/src/service/EntityQueueService.js
@@ -31,6 +31,7 @@ class EntityQueueService extends BaseService {
             .filtered("entity = $0", entityMetadata.entityName)
             .sorted("savedAt")
             .slice(), 'entityUUID');
+
         const getEntity = ({entityUUID, entity}) => this.findByKey("uuid", entityUUID, entity);
         return {
             metaData: entityMetadata,

packages/openchs-android/src/service/EntitySyncStatusService.js
@@ -31,12 +31,14 @@ class EntitySyncStatusService extends BaseService {
 
     geAllSyncStatus() {
         const entityQueueService = this.getService(EntityQueueService);
-        const entities = _.map(this.findAll(), entitySyncStatus => ({
-            entityName: entitySyncStatus.entityName,
-            loadedSince: moment(entitySyncStatus.loadedSince).format("DD-MM-YYYY HH:MM:SS"),
-            queuedCount: entityQueueService.getQueuedItemCount(entitySyncStatus.entityName),
-            type: EntityMetaData.findByName(entitySyncStatus.entityName).type
-        }));
+        const entities = _.map(this.findAll(), entitySyncStatus => {
+            return {
+                entityName: entitySyncStatus.entityName,
+                loadedSince: moment(entitySyncStatus.loadedSince).format("DD-MM-YYYY HH:MM:SS"),
+                queuedCount: entityQueueService.getQueuedItemCount(entitySyncStatus.entityName),
+                type: EntityMetaData.findByName(entitySyncStatus.entityName).type
+            }
+        });
         const mediaQueueService = this.getService(MediaQueueService);
         const mediaGroups = _.groupBy(mediaQueueService.findAll(), 'type');
         const mediaEntities = _.map(mediaGroups, (list, mediaType) => ({
@@ -52,7 +54,7 @@ class EntitySyncStatusService extends BaseService {
 
     getLastLoaded() {
         return moment(_.max(this.findAll(EntitySyncStatus.schema.name)
-            .map((entitySyncStatus)=>entitySyncStatus.loadedSince))).format("DD-MM-YYYY HH:MM:SS");
+            .map((entitySyncStatus) => entitySyncStatus.loadedSince))).format("DD-MM-YYYY HH:MM:SS");
     }
 
     setup(entityMetaDataModel) {

packages/openchs-android/src/service/IdentifierAssignmentService.js
@@ -1,0 +1,59 @@
+import BaseService from './BaseService.js'
+import Service from '../framework/bean/Service';
+import {IdentifierAssignment, Concept, EntityQueue, FormElement, ObservationsHolder} from 'openchs-models';
+import _ from "lodash";
+
+@Service("identifierAssignmentService")
+class IdentifierAssignmentService extends BaseService {
+    constructor(db, beanStore) {
+        super(db, beanStore);
+    }
+
+    getSchema() {
+        return IdentifierAssignment.schema.name;
+    }
+
+    getNextIdentifier(identifierSourceUUID) {
+        let filter = `voided = false AND identifierSource.uuid = ${identifierSourceUUID} AND individual = null and programEnrolment = null`;
+        return this.findAll()
+            .filtered(filter)
+            .sorted("assignmentOrder", false)[0];
+    }
+
+    populateIdentifiers(form, observationHolder) {
+        _.each(form.getFormElementsOfType(Concept.dataType.Id), (formElement) => {
+            if (!observationHolder.findObservation(formElement.concept)) {
+                observationHolder.addOrUpdateObservation(formElement.concept,
+                    this.getNextIdentifier(formElement.recordByKey(FormElement.keys.IdSourceUUID).value).identifier);
+            }
+        });
+        return observationHolder;
+    }
+
+    assignPopulatedIdentifiersFromObservations(form, observations, individual, programEnrolment) {
+        const db = this.db;
+        const identifiersToBeSaved = [];
+        const entityQueueItems = [];
+        const observationsHolder = new ObservationsHolder(observations);
+
+        _.each(form.getFormElementsOfType(Concept.dataType.Id), (formElement) => {
+            if (observationsHolder.findObservation(formElement.concept)) {
+                const identifier = this.getNextIdentifier(formElement.recordByKey(FormElement.keys.IdSourceUUID).value);
+                identifier.individual = individual;
+                identifier.programEnrolment = programEnrolment;
+                identifiersToBeSaved.push(identifier);
+                entityQueueItems.push(EntityQueue.create(identifier, IdentifierAssignment.schema.name));
+            }
+        });
+
+        _.each(identifiersToBeSaved, (identifier) => {
+            db.create(IdentifierAssignment.schema.name, identifier, true);
+        });
+
+        _.each(entityQueueItems, (entityQueueItem) => {
+            db.create(EntityQueue.schema.name, entityQueueItem, true)
+        });
+    }
+}
+
+export default IdentifierAssignmentService;

packages/openchs-android/src/service/IndividualService.js
@@ -1,12 +1,12 @@
 import BaseService from "./BaseService.js";
 import Service from "../framework/bean/Service";
-import {EntityQueue, ObservationsHolder, Program} from "openchs-models";
+import {EntityQueue, ObservationsHolder, Program, Concept, ProgramEncounter, ProgramEnrolment} from "openchs-models";
 import _ from 'lodash';
-import ProgramEncounter from "../../../openchs-models/src/ProgramEncounter";
 import moment from 'moment';
-import ProgramEnrolment from "../../../openchs-models/src/ProgramEnrolment";
 import Individual from "openchs-models/src/Individual";
 import MediaQueueService from "./MediaQueueService";
+import IdentifierAssignmentService from "./IdentifierAssignmentService";
+import FormMappingService from "./FormMappingService";
 
 @Service("individualService")
 class IndividualService extends BaseService {
@@ -43,10 +43,12 @@ class IndividualService extends BaseService {
     register(individual) {
         const db = this.db;
         ObservationsHolder.convertObsForSave(individual.observations);
+        let registrationForm = this.getService(FormMappingService).findRegistrationForm();
         this.db.write(() => {
             db.create(Individual.schema.name, individual, true);
             db.create(EntityQueue.schema.name, EntityQueue.create(individual, Individual.schema.name));
             this.getService(MediaQueueService).addMediaToQueue(individual, Individual.schema.name);
+            this.getService(IdentifierAssignmentService).assignPopulatedIdentifiersFromObservations(registrationForm, individual.observations, individual);
         });
     }
 

commit c5f3f19f78fe00db904592e58d6c4daf57ea5791

0000 | fixing registration broken by 37074f5

packages/openchs-android/src/action/individual/IndividualRegisterActions.js
@@ -26,7 +26,7 @@ export class IndividualRegisterActions {
 
         //Populate identifiers much before form elements are hidden or sent to rules.
         //This will enable the value to be used in rules
-        context.get(IdentifierAssignmentService).populateIdentifiers(form, new ObservationsHolder(individual.observations));
+        context.get(IdentifierAssignmentService).populateIdentifiers(state.form, new ObservationsHolder(individual.observations));
 
         const newState = IndividualRegistrationState.createLoadState(state.form, state.genders, individual);
         IndividualRegisterActions.setAgeState(newState);