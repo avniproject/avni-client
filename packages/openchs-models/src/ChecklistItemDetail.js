1c3ccbbea8a2b4d1b9c00b8d3da17b7ce9216b6c
Hiren Thacker <hithacker@gmail.com>
2019-04-16 18:29:50 +0530

    1334 | Vaccinations page do not open intermittently

--- packages/openchs-models/src/ChecklistItemDetail.js
+++ packages/openchs-models/src/ChecklistItemDetail.js
@@ -26,7 +26,7 @@ class ChecklistItemDetail extends BaseEntity {
         }
     };
 
-    static fromResource(checklistItemResource, entityService, entitiesInCurrentPage) {
+    static fromResource(checklistItemResource, entityService, resourcesInCurrentPage) {
         const checklistDetail = entityService.findByKey("uuid", ResourceUtil.getUUIDFor(checklistItemResource, "checklistDetailUUID"), ChecklistDetail.schema.name);
         const form = entityService.findByKey("uuid", ResourceUtil.getUUIDFor(checklistItemResource, "formUUID"), Form.schema.name);
         const concept = entityService.findByKey("uuid", ResourceUtil.getUUIDFor(checklistItemResource, "conceptUUID"), Concept.schema.name);
@@ -44,11 +44,18 @@ class ChecklistItemDetail extends BaseEntity {
         checklistItemDetail.form = form;
         checklistItemDetail.concept = concept;
         const leadDetailUUID = ResourceUtil.getUUIDFor(checklistItemResource, "leadDetailUUID");
-        const createdLeadChecklistItemDetail = entityService.findByKey("uuid", leadDetailUUID, ChecklistItemDetail.schema.name);
-        if (_.isNil(createdLeadChecklistItemDetail)) {
-            checklistItemDetail.dependentOn = entitiesInCurrentPage.find(entity => entity.uuid === leadDetailUUID);
-        } else {
-            checklistItemDetail.dependentOn = createdLeadChecklistItemDetail;
+        if(!_.isNil(leadDetailUUID)) {
+            const createdLeadChecklistItemDetail = entityService.findByKey("uuid", leadDetailUUID, ChecklistItemDetail.schema.name);
+            if (_.isNil(createdLeadChecklistItemDetail)) {
+                let leadDetail = resourcesInCurrentPage.find(entity => entity.uuid === leadDetailUUID);
+                checklistItemDetail.dependentOn = ChecklistItemDetail.fromResource(
+                    leadDetail,
+                    entityService,
+                    resourcesInCurrentPage
+                );
+            } else {
+                checklistItemDetail.dependentOn = createdLeadChecklistItemDetail;
+            }
         }
         return checklistItemDetail;
     }
