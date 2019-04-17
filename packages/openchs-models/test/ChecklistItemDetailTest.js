aa39e11024dc6544d30adb43f4b1a275c3844727
Hiren Thacker <hithacker@gmail.com>
2019-04-17 10:40:46 +0530

    1334 | refactoring unit test

--- packages/openchs-models/test/ChecklistItemDetailTest.js
+++ packages/openchs-models/test/ChecklistItemDetailTest.js
@@ -1,14 +1,12 @@
 import ChecklistItemDetail from "../src/ChecklistItemDetail";
 
 describe("ChecklistItemTest", () => {
+    const DEPENDENCY_UUID = "2a0956d1-1538-4459-90c4-becf75cdd27c";
+    const DEPENDENT_UUID = "84812dd5-2a4a-4c0d-918d-995f720f15a0";
+    let dependent, dependency, entityService;
 
-    it("fromResource sets depedencies properly regardless of their order in a single page", () => {
-        const entityService = {
-            findByKey: jest.fn().mockReturnValue(null)
-        };
-        const DEPENDENCY_UUID = "2a0956d1-1538-4459-90c4-becf75cdd27c";
-        const DEPENDENT_UUID = "84812dd5-2a4a-4c0d-918d-995f720f15a0";
-        const dependentResource = {
+    beforeEach(() => {
+        dependent = {
             "uuid": DEPENDENT_UUID,
             "_links": {
                 "leadDetailUUID": {
@@ -16,17 +14,21 @@ describe("ChecklistItemTest", () => {
                 }
             }
         };
+        dependency = {
+            "uuid": DEPENDENCY_UUID,
+            "_links": {}
+        };
+        entityService = {
+            findByKey: jest.fn().mockReturnValue(null)
+        };
+    })
 
-        const resources = [
-            dependentResource,
-            {
-                "uuid": DEPENDENCY_UUID,
-                "_links": {}
-            }
+    it("fromResource sets dependencies properly regardless of their order in a single page", () => {
+        const resourcesInCurrentPage = [
+            dependent,
+            dependency
         ];
-
-
-        const dependentItemDetail = ChecklistItemDetail.fromResource(dependentResource, entityService, resources);
+        const dependentItemDetail = ChecklistItemDetail.fromResource(dependent, entityService, resourcesInCurrentPage);
         expect(dependentItemDetail.dependentOn).toBeDefined();
         expect(dependentItemDetail.dependentOn).toBeInstanceOf(ChecklistItemDetail);
     });
