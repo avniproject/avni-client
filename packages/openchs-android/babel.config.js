f20487810ec38c35508f2c47fc4bd0bd9b843057
Vinod Deolal <vinoddeolal@gmail.com>
2019-05-21 12:12:46 +0530

    0000| remove plugin-proposal-class-properties to support SectionList. This plugin is already included in metro

--- packages/openchs-android/babel.config.js
+++ packages/openchs-android/babel.config.js
@@ -7,8 +7,7 @@ module.exports = function (api) {
         "module:metro-react-native-babel-preset"
     ];
     const plugins = [
-            ["@babel/plugin-proposal-decorators", { "legacy": true }],
-            ["@babel/plugin-proposal-class-properties", {"loose": true}]
+            ["@babel/plugin-proposal-decorators", { "legacy": true }]
         ]
     ;
 
