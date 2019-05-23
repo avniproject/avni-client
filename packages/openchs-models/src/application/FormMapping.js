5332283af0acf15a703e7ae0287f61765342378c
Vinay Venu <ijkvinay@gmail.com>
2019-05-23 13:18:24 +0530

    1398 | Add SubjectType to FormMapping

--- packages/openchs-models/src/application/FormMapping.js
+++ packages/openchs-models/src/application/FormMapping.js
@@ -1,6 +1,7 @@
 import ResourceUtil from "../utility/ResourceUtil";
 import Form from "./Form";
 import General from "../utility/General";
+import SubjectType from "../SubjectType";
 
 class FormMapping {
     static schema = {
@@ -9,6 +10,7 @@ class FormMapping {
         properties: {
             uuid: 'string',
             form: 'Form',
+            subjectType: 'SubjectType',
             entityUUID: {type: 'string', optional: true},
             observationsTypeEntityUUID: {type:'string', optional: true},
             voided: { type: 'bool', default: false }
@@ -26,11 +28,13 @@ class FormMapping {
 
     static fromResource(resource, entityService) {
         const form = entityService.findByKey("uuid", ResourceUtil.getUUIDFor(resource, "formUUID"), Form.schema.name);
+        const subjectType = entityService.findByKey("uuid", ResourceUtil.getUUIDFor(resource, "subjectTypeUUID"), SubjectType.schema.name);
 
         const formMapping = General.assignFields(resource, new FormMapping(), ["uuid", "voided"]);
         formMapping.entityUUID = ResourceUtil.getUUIDFor(resource, "entityUUID");
         formMapping.observationsTypeEntityUUID = ResourceUtil.getUUIDFor(resource, "observationsTypeEntityUUID");
         formMapping.form = form;
+        formMapping.subjectType = subjectType;
 
         return formMapping;
     }
