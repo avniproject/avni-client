936bc0f379259cbd6e923896f514353a75585651
Vinod Deolal <vinoddeolal@gmail.com>
2019-05-23 14:07:21 +0530

    1425 | Start Program page to use section list

--- packages/openchs-android/src/views/program/StartProgramView.js
+++ packages/openchs-android/src/views/program/StartProgramView.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from "react";
-import {Text, TouchableNativeFeedback, View, ToastAndroid} from "react-native";
+import {Text, TouchableNativeFeedback, View, ToastAndroid, SectionList, StyleSheet} from "react-native";
 import AbstractComponent from "../../framework/view/AbstractComponent";
 import Path from "../../framework/routing/Path";
 import {StartProgramActionsNames as Actions} from "../../action/program/StartProgramActions"
@@ -15,6 +15,11 @@ import AppHeader from "../common/AppHeader";
 import themes from "../primitives/themes";
 import Distances from "../primitives/Distances";
 import General from "../../utility/General";
+import _ from "lodash";
+import {ProgramEncounter} from "openchs-models";
+import Colors from "../primitives/Colors";
+import Fonts from "../primitives/Fonts";
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 @Path('/StartProgramView')
 class StartProgramView extends AbstractComponent {
@@ -36,92 +41,71 @@ class StartProgramView extends AbstractComponent {
         return super.componentWillMount();
     }
 
-    renderOption(option, idx) {
-        return option ? <PresetOptionItem displayText={option.label}
-                                          checked={option.selected}
-                                          multiSelect={false}
-                                          onPress={() => this.dispatchAction(Actions.ON_SELECTION_CHANGE, option)}
-                                          key={idx}
-                                          style={{paddingTop: Styles.VerticalSpacingBetweenOptionItems, flex: 0.5,
-                                              paddingRight: Distances.HorizontalSpacingBetweenOptionItems}}/>
-            : <View key={idx} style={{flex: 0.5}}/>;
+    encounterFromEncounterType(encounterType) {
+        const programEncounter = ProgramEncounter.createEmptyInstance();
+        programEncounter.programEnrolment = this.state.enrolment;
+        programEncounter.encounterDateTime = moment().toDate();
+        programEncounter.encounterType = encounterType;
+        return programEncounter;
     }
 
-    renderRadioGroup(heading, items) {
-        if (!_.isEmpty(items)) {
-            return (
-                <View style={{
-                    paddingHorizontal: Styles.ContentDistanceFromEdge,
-                    paddingVertical: Styles.VerticalSpacingBetweenOptionItems,
-                    marginVertical: Styles.VerticalSpacingBetweenFormElements,
-                    backgroundColor: '#ffffff',
-                    borderWidth: 1,
-                    borderStyle: 'dashed',
-                    borderRadius: 1,
-                    borderColor: Styles.InputBorderNormal,
-                }}>
-                    <Text style={Styles.formGroupLabel}>{heading}</Text>
-                    {_.chunk(items, 2).map((options, idx) => {
-                        return (
-                            <View style={{flexDirection: 'row'}} key={idx}>
-                                {options.map(this.renderOption.bind(this))}
-                            </View>
-                        )
-                    })}
-                </View>
-
-            );
-        }
-    }
-
-    proceed() {
-        if (!this.state.selectedEncounter) {
-            ToastAndroid.showWithGravity(this.I18n.t("pleaseChooseAOption"), ToastAndroid.SHORT, ToastAndroid.TOP);
-            return;
-        };
-
-        let programEncounter = this.state.selectedEncounter.cloneForEdit();
+    proceed(encounter) {
+        const selectedEncounter = _.has(encounter, 'operationalEncounterTypeName') ? this.encounterFromEncounterType(encounter) : encounter;
+        let programEncounter = selectedEncounter.cloneForEdit();
         programEncounter.encounterDateTime = moment().toDate();
         CHSNavigator.navigateToProgramEncounterView(this, programEncounter);
     }
 
-    renderConjunction(previousList) {
-        if (_.isEmpty(previousList)) return;
-        return <Text style={[{alignSelf: 'center'}, Styles.formGroupLabel]}>{this.I18n.t("or")}</Text>
+    renderHeader(title) {
+        return (<Text style={[Fonts.typography("paperFontTitle"), styles.headerStyle]}>{title}</Text>);
+    }
+
+    renderItem(encounter) {
+        const encounterName = this.I18n.t(encounter.name || encounter.encounterType.name);
+        const displayDate = encounter.earliestVisitDateTime && General.toDisplayDate(encounter.earliestVisitDateTime) || '';
+        const color = moment().isAfter(encounter.maxVisitDateTime) ? '#d0011b' : moment().isBetween(encounter.earliestVisitDateTime, encounter.maxVisitDateTime) ? Colors.AccentColor : 'gold';
+        return (
+            <TouchableNativeFeedback onPress={() => {
+                this.proceed(encounter)
+            }} background={TouchableNativeFeedback.SelectableBackground()}>
+                <View style={styles.container}>
+                    <View style={[styles.strip, !_.isEmpty(displayDate) && {backgroundColor: color}]}/>
+                    <View style={styles.textContainer}>
+                        <Text
+                            style={[Fonts.typography("paperFontSubhead"), styles.encounterStyle]}>{encounterName}</Text>
+                        {!_.isEmpty(displayDate) ?
+                            <Text
+                                style={[Fonts.typography("paperFontSubhead"), styles.dateStyle]}>{displayDate}</Text> :
+                            <View style={{marginLeft: 'auto'}}/>}
+                    </View>
+                    <Icon style={[styles.iconStyle, !_.isEmpty(displayDate) && {color: color}]} name='chevron-right'/>
+                </View>
+            </TouchableNativeFeedback>
+        )
     }
 
     render() {
         General.logDebug(this.viewName(), 'render');
+        const types = !this.state.hideUnplanned ? (!_.isEmpty(this.state.encounters) ? [this.I18n.t("plannedVisits"), this.I18n.t("unplannedVisits")] : [this.I18n.t("unplannedVisits")]) : [this.I18n.t("plannedVisits")];
+        const visits = !this.state.hideUnplanned ? (!_.isEmpty(this.state.encounters) ? [this.state.encounters, this.state.encounterTypes] : [this.state.encounterTypes]) : [this.state.encounters];
+        const data = _.zip(types, visits).map(([t, v]) => ({title: t, data: v}));
 
         return (
             <CHSContainer>
                 <CHSContent>
                     <AppHeader title={this.I18n.t("chooseVisit")}/>
-
-                    <View style={{paddingHorizontal: Styles.ContentDistanceFromEdge,
-                        paddingVertical: Styles.VerticalSpacingBetweenFormElements}}>
-
-                        {this.renderRadioGroup(this.I18n.t("plannedVisits"), this.state.encounters)}
-
-                        {!this.state.hideUnplanned ? this.renderConjunction(this.state.encounters): <View/>}
-
-                        {!this.state.hideUnplanned ? this.renderRadioGroup(this.I18n.t("unplannedVisits"), this.state.encounterTypes) : <View/>}
-
-                        <View style={{flexDirection: 'row', justifyContent: 'flex-end', marginTop: 16}}>
-                            <TouchableNativeFeedback onPress={() => { CHSNavigator.goBack(this) }}
-                                                     background={TouchableNativeFeedback.SelectableBackground()}>
-                                <View style={Styles.basicSecondaryButtonView}>
-                                    <Text style={{color: Styles.blackColor, fontSize: 16}}>{this.I18n.t("cancel")}</Text>
-                                </View>
-                            </TouchableNativeFeedback>
-
-                            <TouchableNativeFeedback onPress={() => { this.proceed() }}
-                                                     background={TouchableNativeFeedback.SelectableBackground()}>
-                                <View style={[Styles.basicPrimaryButtonView, {marginHorizontal: 16, }]}>
-                                    <Text style={{color: Styles.whiteColor, fontSize: 16}}>{this.I18n.t("proceed")}</Text>
-                                </View>
-                            </TouchableNativeFeedback>
-                        </View>
+                    <View>
+                        <SectionList
+                            contentContainerStyle={{
+                                marginRight: Distances.ScaledContentDistanceFromEdge,
+                                marginLeft: Distances.ScaledContentDistanceFromEdge,
+                                marginTop: Distances.ScaledContentDistanceFromEdge
+                            }}
+                            sections={data}
+                            renderSectionHeader={({section: {title}}) => this.renderHeader(title)}
+                            renderItem={(data) => this.renderItem(data.item.data)}
+                            keyExtractor={(item, index) => index}
+                        />
                     </View>
                 </CHSContent>
             </CHSContainer>
@@ -130,3 +114,56 @@ class StartProgramView extends AbstractComponent {
 }
 
 export default StartProgramView;
+
+const styles = StyleSheet.create({
+        container: {
+            margin: 4,
+            elevation: 2,
+            minHeight: 48,
+            marginVertical: 8,
+            backgroundColor: '#fefefe',
+            flexDirection: 'row',
+            alignItems: 'center',
+            alignSelf: 'center',
+        },
+        strip: {
+            width: 8,
+            height: '100%',
+            backgroundColor: Colors.GreyBackground
+        },
+        textContainer: {
+            flex: 1,
+            paddingVertical: 4,
+            padding: Distances.ScaledContentDistanceFromEdge,
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+        },
+        encounterStyle: {
+            color: Colors.DefaultPrimaryColor,
+            fontWeight: 'normal',
+            fontSize: 13,
+            alignSelf: 'flex-start',
+            textAlignVertical: 'center',
+        },
+        dateStyle: {
+            color: Styles.greyText,
+            fontWeight: 'normal',
+            fontSize: 13,
+            alignSelf: 'flex-end',
+            marginLeft: 'auto',
+            textAlignVertical: 'center',
+        },
+        headerStyle: {
+            color: "rgba(0, 0, 0, 0.87)",
+            fontWeight: 'normal',
+            fontSize: 15,
+            paddingTop: 15
+        },
+        iconStyle: {
+            color: Colors.GreyBackground,
+            opacity: 0.8,
+            alignSelf: 'center',
+            fontSize: 40
+        }
+    }
+);
