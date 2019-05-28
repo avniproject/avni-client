534fbffc2ec76dbd95fedd2db6dd30833f08c272
sidtharthan <sidthunagarajan@gmail.com>
2019-05-28 18:28:18 +0530

    1434 | Introduce ConfirmationModal component
    Alert.alert has limited styling. So added this component.

--- packages/openchs-android/src/views/primitives/Colors.js
+++ packages/openchs-android/src/views/primitives/Colors.js
@@ -12,6 +12,7 @@ class Colors {
 
     static ValidationError = '#d0011b';
     static AbnormalValueHighlight = '#d0011b';
+    static NegativeActionButtonColor = '#d0011b';
     static InputNormal = 'rgba(0, 0, 0, 0.87)';
     static InputBorderNormal = 'rgba(0, 0, 0, 0.12)';
     static GreyBackground = 'rgba(97, 97, 97, 0.20)';
