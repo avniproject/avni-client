c4201d3ca3070506fb53967ea39b36b6a69a78b9
Vinod Deolal <vinoddeolal@gmail.com>
2019-08-08 15:13:13 +0530

    #181 | Do not crash the application if rules fail on client side

--- packages/openchs-android/src/service/RuleEvaluationService.js
+++ packages/openchs-android/src/service/RuleEvaluationService.js
@@ -9,7 +9,9 @@ import {
     Individual,
     Observation,
     ProgramEncounter,
-    ProgramEnrolment
+    ProgramEnrolment,
+    Rule,
+    RuleFailureTelemetry,
 } from 'openchs-models';
 import {
     encounterDecision,
@@ -27,7 +29,7 @@ import General from "../utility/General";
 import RuleService from "./RuleService";
 import IndividualService from "./IndividualService";
 import IndividualEncounterService from "./IndividualEncounterService";
-import {Rule} from 'openchs-models';
+import EntityService from "./EntityService";
 
 @Service("ruleEvaluationService")
 class RuleEvaluationService extends BaseService {
@@ -59,7 +61,24 @@ class RuleEvaluationService extends BaseService {
         this.conceptService = this.getService(ConceptService);
     }
 
-    getEntityDecision(form, entity, context) {
+    getIndividualUUID = (entity, entityName) => {
+        switch (entityName) {
+            case 'Individual':
+                return entity.uuid;
+            case 'ProgramEnrolment':
+                return entity.individual.uuid;
+            case 'ProgramEncounter':
+                return entity.programEnrolment.individual.uuid;
+            case 'Encounter' :
+                return entity.individual.uuid;
+            case 'WorkList':
+                return entity.getCurrentWorkItem().parameters.subjectUUID;
+            default :
+                return "entity not mapped";
+        }
+    };
+
+    getEntityDecision(form, entity, context, entityName) {
         const defaultDecisions = {
             "enrolmentDecisions": [],
             "encounterDecisions": [],
@@ -67,7 +86,10 @@ class RuleEvaluationService extends BaseService {
         };
         if ([form, entity].some(_.isEmpty)) return defaultDecisions;
         const decisionsMap = this.getAllRuleItemsFor(form, "Decision", "Form")
-            .reduce((decisions, rule) => rule.fn.exec(entity, decisions, context, new Date()), defaultDecisions);
+            .reduce((decisions, rule) => {
+                const d = this.runRuleAndSaveFailure(rule, entityName, entity, decisions, new Date(), context);
+                return this.validateDecisions(d, rule.uuid, this.getIndividualUUID(entity, entityName));
+            }, defaultDecisions);
         const trimmedDecisions = {};
         _.forEach(decisionsMap, (decisions, decisionType) => {
             trimmedDecisions[decisionType] = _.reject(decisions, _.isEmpty);
@@ -76,21 +98,82 @@ class RuleEvaluationService extends BaseService {
         return trimmedDecisions;
     }
 
+    //this is required because we check for the concept name after generating decisions
+    validateDecisions(d, ruleUUID, individualUUID) {
+        return _.merge(..._.map(d, (decisions, decisionType) => {
+            return {
+                [decisionType]: decisions.map(obj => this.filterValues(obj, ruleUUID, individualUUID))
+                    .filter(obj => this.checkConceptForRule(obj.name, ruleUUID, individualUUID))
+            }
+        }));
+    }
+
+    filterValues(object, ruleUUID, individualUUID) {
+        object.value = object.value.filter(conceptName => this.checkConceptForRule(conceptName, ruleUUID, individualUUID));
+        return object;
+    }
+
+    checkConceptForRule(conceptName, ruleUUID, individualUUID) {
+        try {
+            this.conceptService.findConcept(conceptName);
+            return true;
+        } catch (error) {
+            this.saveFailedRules(error, ruleUUID, individualUUID);
+            return false;
+        }
+    }
+
     getDecisions(entity, entityName, context) {
-        const formMapKey = _.get(context, 'usage') === 'Exit' ? 'ProgramExit': entityName;
-        const form = this.entityFormMap.get(formMapKey)(entity)
-        return this.getEntityDecision(form, entity, context);
+        const formMapKey = _.get(context, 'usage') === 'Exit' ? 'ProgramExit' : entityName;
+        const form = this.entityFormMap.get(formMapKey)(entity);
+        return this.getEntityDecision(form, entity, context, entityName);
     }
 
     updateWorkLists(workLists, context) {
         const additionalRules = this.getService(RuleService).getRulesByType('WorkListUpdation');
-        return _.reduce(additionalRules, (newWorkLists, rule) => rule.fn.exec(workLists, context), workLists);
+        return _.reduce(additionalRules, (newWorkLists, rule) => this.runRuleAndSaveFailure(rule, 'WorkList', workLists, null, null, context), workLists);
+    }
+
+    runRuleAndSaveFailure(rule, entityName, entity, ruleTypeValue, config, context) {
+        try {
+            if (entityName === 'WorkList') {
+                ruleTypeValue = entity;
+                return rule.fn.exec(entity, context)
+            } else {
+                return _.isNil(context) ?
+                    rule.fn.exec(entity, ruleTypeValue, config) :
+                    rule.fn.exec(entity, ruleTypeValue, context, config);
+            }
+        } catch (error) {
+            General.logDebug("Rule-Failure", `Rule failed: ${rule.name}, uuid: ${rule.uuid}`);
+            this.saveFailedRules(error, rule.uuid, this.getIndividualUUID(entity, entityName));
+            return ruleTypeValue;
+        }
+    }
+
+    saveFailedRules(error, ruleUUID, individualUUID) {
+        const entityService = this.getService(EntityService);
+        let ruleFailureTelemetry = RuleFailureTelemetry.create({
+            errorMessage: error.message,
+            stacktrace: error.stack,
+            ruleUUID: ruleUUID,
+            individualUUID: individualUUID,
+        });
+        entityService.saveAndPushToEntityQueue(ruleFailureTelemetry, RuleFailureTelemetry.schema.name);
+    }
+
+    //check if summary name is present in concepts
+    validateSummaries(s, ruleUUID, individualUUID) {
+        return s.filter(obj => this.checkConceptForRule(obj.name, ruleUUID, individualUUID))
     }
 
     getEnrolmentSummary(enrolment, entityName = 'ProgramEnrolment', context) {
         const summaries = this.entityRulesMap.get(entityName).getEnrolmentSummary(enrolment, context);
         const updatedSummaries = this.getAllRuleItemsFor(enrolment.program, "EnrolmentSummary", "Program")
-            .reduce((summaries, rule) => rule.fn.exec(enrolment, summaries, context, new Date()), summaries);
+            .reduce((summaries, rule) => {
+                const s = this.runRuleAndSaveFailure(rule, entityName, enrolment, summaries, new Date(), context);
+                return this.validateSummaries(s, rule.uuid, this.getIndividualUUID(enrolment, entityName));
+            }, summaries);
         const conceptFor = name => this.conceptService.conceptFor(name);
         const summaryObservations = _.map(updatedSummaries, (summary) => {
             const concept = conceptFor(summary.name);
@@ -104,7 +187,7 @@ class RuleEvaluationService extends BaseService {
         const defaultValidationErrors = [];
         if ([entity, form].some(_.isEmpty)) return defaultValidationErrors;
         const validationErrors = this.getAllRuleItemsFor(form, "Validation", "Form")
-            .reduce((validationErrors, rule) => rule.fn.exec(entity, validationErrors), defaultValidationErrors);
+            .reduce((validationErrors, rule) => this.runRuleAndSaveFailure(rule, entityName, entity, validationErrors), defaultValidationErrors);
         General.logDebug("RuleEvaluationService - Validation Errors", validationErrors);
         return validationErrors;
     }
@@ -117,7 +200,7 @@ class RuleEvaluationService extends BaseService {
         const nextVisits = this.getAllRuleItemsFor(form, "VisitSchedule", "Form")
             .reduce((schedule, rule) => {
                 General.logDebug(`RuleEvaluationService`, `Executing Rule: ${rule.name} Class: ${rule.fnName}`);
-                return rule.fn.exec(entity, schedule, visitScheduleConfig)
+                return this.runRuleAndSaveFailure(rule, entityName, entity, schedule, visitScheduleConfig)
             }, scheduledVisits);
         General.logDebug("RuleEvaluationService - Next Visits", nextVisits);
         return nextVisits;
@@ -128,7 +211,7 @@ class RuleEvaluationService extends BaseService {
         const allChecklistDetails = this.findAll(ChecklistDetail.schema.name);
         if ([entity, form, allChecklistDetails].some(_.isEmpty)) return defaultChecklists;
         const allChecklists = this.getAllRuleItemsFor(form, "Checklists", "Form")
-            .reduce((checklists, rule) => rule.fn.exec(entity, allChecklistDetails), defaultChecklists);
+            .reduce((checklists, rule) => this.runRuleAndSaveFailure(rule, entityName, entity, allChecklistDetails), defaultChecklists);
         // General.logDebug("RuleEvaluationService - Checklists", allChecklists);
         return allChecklists;
     }
@@ -140,7 +223,7 @@ class RuleEvaluationService extends BaseService {
             .map((formElement) => new FormElementStatus(formElement.uuid, true, undefined));
         if (_.isEmpty(allRules)) return defaultFormElementStatus;
         return [...allRules
-            .map(r => r.fn.exec(entity, formElementGroup, new Date()))
+            .map(r => this.runRuleAndSaveFailure(r, entityName, entity, formElementGroup, new Date()))
             .reduce((all, curr) => all.concat(curr), defaultFormElementStatus)
             .reduce((acc, fs) => acc.set(fs.uuid, fs), new Map())
             .values()];
@@ -157,12 +240,12 @@ class RuleEvaluationService extends BaseService {
 
     isEligibleForEncounter(individual, encounterType) {
         const applicableRules = this.getAllRuleItemsFor(encounterType, "EncounterEligibilityCheck", "EncounterType");
-        return _.isEmpty(applicableRules) ? true : _.last(applicableRules).fn.exec({individual});
+        return _.isEmpty(applicableRules) ? true : this.runRuleAndSaveFailure(_.last(applicableRules), 'Encounter', {individual}, true);
     }
 
     isEligibleForProgram(individual, program) {
         const applicableRules = this.getAllRuleItemsFor(program, "EnrolmentEligibilityCheck", "Program");
-        return _.isEmpty(applicableRules) ? true : _.last(applicableRules).fn.exec({individual});
+        return _.isEmpty(applicableRules) ? true : this.runRuleAndSaveFailure(_.last(applicableRules), 'Encounter', {individual}, true);
     }
 
     runOnAll(rulesToRun) {
