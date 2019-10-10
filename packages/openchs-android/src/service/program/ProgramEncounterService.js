738d05ed9ebf137d2eda8e916daf1799d3ea0375
Vinay Venu <ijkvinay@gmail.com>
2019-10-10 13:38:43 +0530

    #205 | Add new parameter to visitSchedule rule response that will create new visits if the rules demand it

--- packages/openchs-android/src/service/program/ProgramEncounterService.js
+++ packages/openchs-android/src/service/program/ProgramEncounterService.js
@@ -42,10 +42,12 @@ class ProgramEncounterService extends BaseService {
     }
 
     saveScheduledVisit(enrolment, nextScheduledVisit, db, schedulerDate) {
-        let encountersToUpdate = enrolment.scheduledEncountersOfType(nextScheduledVisit.encounterType);
-        if (_.isEmpty(encountersToUpdate)) {
-            const encounterType = this.findByKey('name', nextScheduledVisit.encounterType, EncounterType.schema.name);
-            if (_.isNil(encounterType)) throw Error(`NextScheduled visit is for encounter type=${nextScheduledVisit.encounterType} that doesn't exist`);
+        const {encounterType: encounterTypeName, visitCreationStrategy = 'default'} = nextScheduledVisit;
+
+        let encountersToUpdate = enrolment.scheduledEncountersOfType(encounterTypeName);
+        if (_.isEmpty(encountersToUpdate) || visitCreationStrategy === 'createNew') {
+            const encounterType = this.findByKey('name', encounterTypeName, EncounterType.schema.name);
+            if (_.isNil(encounterType)) throw Error(`NextScheduled visit is for encounter type=${encounterTypeName} that doesn't exist`);
             encountersToUpdate = [ProgramEncounter.createScheduled(encounterType, enrolment)];
         }
         _.forEach(encountersToUpdate, enc => this._saveEncounter(enc.updateSchedule(nextScheduledVisit), db));
