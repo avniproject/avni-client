dc772328656267c3471d88e77e71232d65cacaac
sachin kadam <sachinkadamsk01@gmail.com>
2020-02-10 15:12:39 +0530

    0000 | Growth chart option added for phulwari

--- packages/openchs-health-modules/health_modules/programConfig.js
+++ packages/openchs-health-modules/health_modules/programConfig.js
@@ -2,6 +2,7 @@ import childProgramConfig from "./child/childProgramConfig";
 
 const programConfigExports = {};
 programConfigExports.Child = childProgramConfig;
+programConfigExports.Phulwari = childProgramConfig;
 
 const config = function (programName) {
     return !programName ? programConfigExports : programConfigExports[programName];
