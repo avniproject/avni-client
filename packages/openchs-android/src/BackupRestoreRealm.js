5a6299b290a6f414f84ddfab5a16c4f97afa2520
Vinod Deolal <vinoddeolal@gmail.com>
2020-03-16 16:16:13 +0530

    #300 | Remove UserInfo from the db backup

--- packages/openchs-android/src/BackupRestoreRealm.js
+++ packages/openchs-android/src/BackupRestoreRealm.js
@@ -4,15 +4,30 @@ import FileSystem from "./model/FileSystem";
 import {ToastAndroid} from 'react-native';
 import General from "./utility/General";
 import fs from 'react-native-fs';
+import {UserInfo} from 'avni-models';
 
 const REALM_FILE_NAME = "default.realm";
 const DESTINATION_FILE_PATH = `${fs.DocumentDirectoryPath}/${REALM_FILE_NAME}`;
 
+const deleteUserInfo = (db) => {
+    db.write(() => {
+        const objects = db.objects(UserInfo.schema.name);
+        db.delete(objects);
+    });
+};
+
+const restoreUserInfo = (db, userInfo) => {
+    db.write(() => db.create(UserInfo.schema.name, userInfo, 'modified'));
+};
+
 export const backup = (fileName) => {
     const db = new Realm(Schema);
+    const userInfo = _.assign({}, db.objects(UserInfo.schema.name)[0]);
     fs.readDir(FileSystem.getBackupDir())
         .then((files) => files.forEach(file => fs.unlink(file.path)))
+        .then(() => deleteUserInfo(db))
         .then(() => db.writeCopyTo(`${FileSystem.getBackupDir()}/${fileName}`))
+        .then(() => restoreUserInfo(db, userInfo))
         .then(() => ToastAndroid.show('Backup Complete', ToastAndroid.SHORT))
         .catch((error) => {
             General.logError(`Error while taking backup, ${error.message}`);
