57187fa43606a1251a9602641fc75699cf5b64ea
Vinod Deolal <vinoddeolal@gmail.com>
2020-09-10 15:39:29 +0530

    #375 | remove user identifiers before creating DB backup right now if another user restores the backup he ends up using the reamining identifiers of the user who created the backup

--- packages/openchs-android/src/BackupRestoreRealm.js
+++ packages/openchs-android/src/BackupRestoreRealm.js
@@ -1,33 +1,49 @@
-import {Schema} from 'avni-models';
+import {EntitySyncStatus, IdentifierAssignment, Schema, UserInfo} from 'avni-models';
 import Realm from 'realm';
 import FileSystem from "./model/FileSystem";
 import {ToastAndroid} from 'react-native';
 import General from "./utility/General";
 import fs from 'react-native-fs';
-import {UserInfo} from 'avni-models';
 
 const REALM_FILE_NAME = "default.realm";
 const DESTINATION_FILE_PATH = `${fs.DocumentDirectoryPath}/${REALM_FILE_NAME}`;
 
-const deleteUserInfo = (db) => {
+const deleteUserInfoAndIdAssignment = (db, entitySyncStatus) => {
     db.write(() => {
         const objects = db.objects(UserInfo.schema.name);
+        const assignmentObjects = db.objects(IdentifierAssignment.schema.name);
         db.delete(objects);
+        db.delete(assignmentObjects);
+        entitySyncStatus.forEach(({uuid, entityName, entityTypeUuid}) => {
+            const updatedEntity = EntitySyncStatus.create(entityName, EntitySyncStatus.REALLY_OLD_DATE, uuid, entityTypeUuid);
+            db.create(EntitySyncStatus.schema.name, updatedEntity, true);
+        })
     });
 };
 
-const restoreUserInfo = (db, userInfo) => {
-    db.write(() => db.create(UserInfo.schema.name, userInfo, 'modified'));
+const restoreDeletedOrUpdatedEntities = (db, userInfo, entitySyncStatus, identifierAssignment) => {
+    db.write(() => {
+        _.forEach(userInfo, ui => db.create(UserInfo.schema.name, ui, true));
+        _.forEach(entitySyncStatus, ({uuid, entityName, loadedSince, entityTypeUuid}) => {
+            const updatedEntity = EntitySyncStatus.create(entityName, loadedSince, uuid, entityTypeUuid);
+            db.create(EntitySyncStatus.schema.name, updatedEntity, true)
+        });
+        _.forEach(identifierAssignment, ida => db.create(IdentifierAssignment.schema.name, ida, true));
+    });
 };
 
 export const backup = (fileName) => {
     const db = new Realm(Schema);
-    const userInfo = _.assign({}, db.objects(UserInfo.schema.name)[0]);
+    const userInfo = db.objects(UserInfo.schema.name).map(u => _.assign({}, u));
+    const entitySyncStatus = db.objects(EntitySyncStatus.schema.name)
+        .filtered(`entityName = 'IdentifierAssignment'`)
+        .map(u => _.assign({}, u));
+    const identifierAssignment = db.objects(IdentifierAssignment.schema.name).map(u => _.assign({}, u));
     fs.readDir(FileSystem.getBackupDir())
         .then((files) => files.forEach(file => fs.unlink(file.path)))
-        .then(() => deleteUserInfo(db))
+        .then(() => deleteUserInfoAndIdAssignment(db, entitySyncStatus))
         .then(() => db.writeCopyTo(`${FileSystem.getBackupDir()}/${fileName}`))
-        .then(() => restoreUserInfo(db, userInfo))
+        .then(() => restoreDeletedOrUpdatedEntities(db, userInfo, entitySyncStatus, identifierAssignment))
         .then(() => ToastAndroid.show('Backup Complete', ToastAndroid.SHORT))
         .catch((error) => {
             General.logError(`Error while taking backup, ${error.message}`);
