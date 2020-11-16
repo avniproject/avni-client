d901afc2e996fb99742970985018305f795a58c6
Joy <itsjoy@gmail.com>
2020-11-16 14:37:47 +0530

    #322 | UI fixes for scheduled visits for others

--- packages/openchs-android/src/views/common/NextScheduledVisitsForOtherSubjects.js
+++ packages/openchs-android/src/views/common/NextScheduledVisitsForOtherSubjects.js
@@ -5,7 +5,6 @@ import {SectionList, StyleSheet, Text, View} from "react-native";
 import Fonts from "../primitives/Fonts";
 import Styles from "../primitives/Styles";
 import React from "react";
-import Distances from "../primitives/Distances";
 import moment from "moment";
 import _ from "lodash";
 
@@ -13,7 +12,6 @@ import _ from "lodash";
 class NextScheduledVisitsForOtherSubjects extends AbstractComponent {
     static propTypes = {
         nextScheduledVisits: PropTypes.any.isRequired,
-        // sectionSeparator: PropTypes.string,
         style: PropTypes.object,
         title: PropTypes.string
     };
@@ -21,7 +19,6 @@ class NextScheduledVisitsForOtherSubjects extends AbstractComponent {
     constructor(props, context) {
         super(props, context);
         this.createScheduledVisitStyles(props.highlight);
-        // this.state = {sliceCount: 2};
     }
 
     createScheduledVisitStyles(highlight) {
@@ -31,14 +28,16 @@ class NextScheduledVisitsForOtherSubjects extends AbstractComponent {
                     marginHorizontal: 3,
                     backgroundColor: Colors.GreyContentBackground
                 },
-                visitRow: {borderRightWidth: 1, borderColor: 'rgba(0, 0, 0, 0.12)'},
+                visitRow: {marginRight: 3, borderRightWidth: 1, borderColor: 'rgba(0, 0, 0, 0.12)'},
                 visitColumn: {
                     borderLeftWidth: 1,
                     borderColor: 'rgba(0, 0, 0, 0.12)',
                     paddingLeft: 3,
+                    paddingRight: 3,
                     paddingBottom: 2,
                     textAlign: 'center',
-                    flex: 1
+                    flex: 1,
+                    marginHorizontal: 3
                 },
                 container: {
                     flex: 1,
@@ -47,6 +46,15 @@ class NextScheduledVisitsForOtherSubjects extends AbstractComponent {
                 tableHeaderRow: {
                     marginHorizontal: 3,
                     marginTop: 4,
+                    flexDirection: "row",
+                    borderBottomColor: Colors.InputBorderNormal,
+                    borderBottomWidth: StyleSheet.hairlineWidth,
+                    flexWrap: 'wrap',
+                    backgroundColor: Colors.HighlightBackgroundColor
+                },
+                tableFooterRow: {
+                    marginHorizontal: 3,
+                    marginBottom: 4,
                     flexDirection: "row",
                     borderBottomColor: Colors.InputBorderNormal,
                     borderBottomWidth: StyleSheet.hairlineWidth,
@@ -63,6 +71,21 @@ class NextScheduledVisitsForOtherSubjects extends AbstractComponent {
                     color: Styles.greyText,
                     fontWeight: 'bold',
                     paddingTop: 5
+                },
+                tableSectionHeader: {
+                    marginHorizontal: 3,
+                    borderLeftWidth: 1,
+                    borderRightWidth: 1,
+                    borderBottomWidth: 1,
+                    borderColor: 'rgba(0, 0, 0, 0.12)',
+                    paddingLeft: 3,
+                    paddingTop: 2,
+                    paddingBottom: 2,
+                    textAlign: 'center',
+                    fontSize: Fonts.Normal,
+                    color: Styles.greyText,
+                    fontWeight: 'bold',
+                    backgroundColor: Colors.GreyBackground
                 }
             }
     }
@@ -72,46 +95,34 @@ class NextScheduledVisitsForOtherSubjects extends AbstractComponent {
     }
 
     render() {
-        // console.log(`nextScheduledVisits: ${JSON.stringify(util.inspect(this.props.nextScheduledVisits, {depth: 2}))}`);
         if (_.isEmpty(this.props.nextScheduledVisits)) return <View/>;
 
-        const visitsBySubject = _.toArray(_.groupBy(this.props.nextScheduledVisits, 'subject.uuid')).map((value) => ({
+        const visitsBySubject = _.sortBy(_.toArray(_.groupBy(this.props.nextScheduledVisits, 'subject.uuid')).map((value) => ({
             title: value[0].subject.name,
             data: value
-        }));
-        // console.log(`sections: ${JSON.stringify(util.inspect(visitsBySubject, {depth: 2}))}`);
+        })), (visit) => visit.title.toLowerCase());
         const dateFormat = "DD-MMM-YYYY";
 
         return (
-            <View style={[{flexDirection: "column", paddingBottom: 10}, this.props.style]}>
+            <View style={[{flexDirection: "column", paddingBottom: 10}, /*this.styles.visitTable,*/ this.props.style]}>
                 {this.renderTitle()}
                 <SectionList
                     ListHeaderComponent={
                         <View style={[this.styles.tableHeaderRow]}>
                             <Text style={[this.styles.tableColHeader, {flex: 1.5,}]}>{this.I18n.t('visitName')}</Text>
-                            <Text
-                                style={[this.styles.tableColHeader, {flex: 1.5,}]}>{this.I18n.t('schedulingFor')}</Text>
+                            <Text style={[this.styles.tableColHeader, {flex: 1.5,}]}>{this.I18n.t('schedulingFor')}</Text>
                             <Text style={[this.styles.tableColHeader, {flex: 1.5,}]}>{this.I18n.t('overdueBy')}</Text>
                         </View>
                     }
-                    contentContainerStyle={{
-                        marginRight: Distances.ScaledContentDistanceFromEdge,
-                        marginLeft: Distances.ScaledContentDistanceFromEdge,
-                        marginTop: Distances.ScaledContentDistanceFromEdge
-                    }}
-                    // sections={visitsBySubject.slice(0, this.state.sliceCount)}
                     sections={visitsBySubject}
                     renderSectionHeader={({section: {title}}) =>
-                        <Text style={[Fonts.typography("paperFontTitle"), {
-                            color: "rgba(0, 0, 0, 0.87)",
-                            fontWeight: 'normal',
-                            fontSize: 15,
-                            paddingTop: 15
-                        }]}>{title}</Text>
+                        <Text style={[this.styles.tableSectionHeader]}>{title}</Text>
                     }
+                    ListFooterComponent={<View style={this.styles.tableFooterRow} />}
+                    stickySectionHeadersEnabled={true}
                     renderItem={({item}) => {
                         return (
-                            < View style={[{flexDirection: "row"}, this.styles.visitRow]}>
+                            <View style={[{flexDirection: "row"}, this.styles.visitRow]}>
                                 <Text style={[{
                                     textAlign: 'left',
                                     fontSize: Fonts.Normal,
@@ -119,23 +130,19 @@ class NextScheduledVisitsForOtherSubjects extends AbstractComponent {
                                 }, this.styles.visitColumn]}>{item.name}</Text>
                                 <Text style={[{
                                     textAlign: 'left',
-                                    fontSize: Fonts.Medium,
+                                    fontSize: Fonts.Normal,
                                 }, this.styles.visitColumn]}>{moment(item.earliestDate).format(dateFormat)}</Text>
                                 <Text style={[{
                                     textAlign: 'left',
                                     fontSize: Fonts.Normal,
-                                    color: Styles.greyText
+                                    color: Styles.greyText,
                                 }, this.styles.visitColumn]}>{moment(item.maxDate).format(dateFormat)}</Text>
                             </View>
                         );
                     }
                     }
                     keyExtractor={(item, index) => index}
-                    // initialNumToRender={1}
-                    // onEndReachedThreshold={0.2}
-                    // onEndReached={() => this.setState({sliceCount: this.state.sliceCount + 1})}
                 />
-
             </View>
         );
     }
