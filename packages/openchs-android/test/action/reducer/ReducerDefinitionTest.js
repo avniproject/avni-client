0cee4085261527c774373ad83aa53a465898ccc5
Vinod Deolal <vinoddeolal@gmail.com>
2021-01-20 12:14:01 +0530

    #385 | fix the test, mock newly added DeviceInfo methods

--- packages/openchs-android/test/action/reducer/ReducerDefinitionTest.js
+++ packages/openchs-android/test/action/reducer/ReducerDefinitionTest.js
@@ -12,6 +12,45 @@ jest.mock("react-native-device-info", () => {
         },
         getDeviceId: function () {
             return 1;
+        },
+        getBrand: function () {
+            return 1;
+        },
+        getManufacturerSync: function () {
+            return 1;
+        },
+        getDeviceType: function () {
+            return 1;
+        },
+        getCarrierSync: function () {
+            return 1;
+        },
+        isEmulatorSync: function () {
+            return 1;
+        },
+        getPowerStateSync: function () {
+            return 1;
+        },
+        getFreeDiskStorageSync: function () {
+            return 1;
+        },
+        getTotalMemorySync: function () {
+            return 1;
+        },
+        getMaxMemorySync: function () {
+            return 1;
+        },
+        isPinOrFingerprintSetSync: function () {
+            return 1;
+        },
+        isLocationEnabledSync: function () {
+            return 1;
+        },
+        getFirstInstallTimeSync: function () {
+            return 1;
+        },
+        getLastUpdateTimeSync: function () {
+            return 1;
         }
     };
 });
