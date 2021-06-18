avniproject/avni-client

commit 238fbbba23ae1b0d8ba137a47bf2ebe905c1a0b6

#404 | Refresh token before every call

packages/openchs-android/src/service/SyncService.js
@@ -7,7 +7,6 @@ import SettingsService from "./SettingsService";
 import {EntityApprovalStatus, EntityMetaData, EntitySyncStatus, RuleFailureTelemetry, SyncTelemetry} from 'avni-models';
 import EntityQueueService from "./EntityQueueService";
 import MessageService from "./MessageService";
-import AuthService from "./AuthService";
 import RuleEvaluationService from "./RuleEvaluationService";
 import MediaQueueService from "./MediaQueueService";
 import ProgressbarStatus from "./ProgressbarStatus";
@@ -38,17 +37,12 @@ class SyncService extends BaseService {
         this.entityService = this.getService(EntityService);
         this.conventionalRestClient = new ConventionalRestClient(this.getService(SettingsService), this.getService(PrivilegeService));
         this.messageService = this.getService(MessageService);
-        this.authService = this.getService(AuthService);
         this.ruleEvaluationService = this.getService(RuleEvaluationService);
         this.mediaQueueService = this.getService(MediaQueueService);
         this.entityQueueService = this.getService(EntityQueueService);
         this.ruleService = this.getService(RuleService);
         this.mediaService = this.getService(MediaService);
         this.newsService = this.getService(NewsService);
-    }
-
-    authenticate() {
-        return this.authService.getAuthToken();
     }
 
     getProgressSteps(allEntitiesMetaData) {
@@ -133,12 +127,8 @@ class SyncService extends BaseService {
     }
 
     imageSync(statusMessageCallBack) {
-        return this.authenticate()
-            .then((idToken) => {
-                statusMessageCallBack("uploadMedia");
-                return idToken;
-            })
-            .then((idToken) => this.mediaQueueService.uploadMedia(idToken));
+        return statusMessageCallBack("uploadMedia")
+            .then(() => this.mediaQueueService.uploadMedia());
     }
 
     telemetrySync(allEntitiesMetaData, onProgressPerEntity) {
@@ -147,9 +137,7 @@ class SyncService extends BaseService {
         const entitiesToPost = telemetryMetadata.reverse()
             .map(this.entityQueueService.getAllQueuedItems)
             .filter((entities) => !_.isEmpty(entities.entities));
-        return this.authenticate()
-            .then((idToken) => this.conventionalRestClient.setToken(idToken))
-            .then(() => this.conventionalRestClient.postAllEntities(entitiesToPost, onCompleteOfIndividualPost, onProgressPerEntity))
+        return this.conventionalRestClient.postAllEntities(entitiesToPost, onCompleteOfIndividualPost, onProgressPerEntity);
     }
 
     dataServerSync(allEntitiesMetaData, statusMessageCallBack, onProgressPerEntity, onAfterMediaPush, updateProgressSteps) {
@@ -157,10 +145,7 @@ class SyncService extends BaseService {
         const allReferenceDataMetaData = allEntitiesMetaData.filter((entityMetaData) => entityMetaData.type === "reference");
         const allTxEntityMetaData = allEntitiesMetaData.filter((entityMetaData) => entityMetaData.type === "tx");
 
-        return this.authenticate()
-            .then((idToken) => this.conventionalRestClient.setToken(idToken))
-
-            .then(() => statusMessageCallBack("uploadLocallySavedData"))
+        return statusMessageCallBack("uploadLocallySavedData")
             .then(() => this.pushData(allTxEntityMetaData.slice(), onProgressPerEntity))
             .then(() => onAfterMediaPush("After_Media", 0))
 

packages/openchs-android/config/env/dev.json.template
@@ -1,5 +1,5 @@
 {
-  "SERVER_URL": "http://SERVER_URL_VAR:8021",
+  "SERVER_URL": "http://10.0.2.2:8021",
   "ENV": "dev",
   "CLIENT_ID": "dummy",
   "goToLastPageOnNext": false,

packages/openchs-android/src/framework/http/ChainedRequests.js
@@ -13,12 +13,12 @@ class ChainedRequests {
         this.requestQueue.push(request);
     }
 
-    get(endpoint, authToken, onComplete) {
-        return () => httpGet(endpoint, authToken).then(onComplete);
+    get(endpoint, onComplete) {
+        return () => httpGet(endpoint).then(onComplete);
     }
 
-    post(endpoint, filecontents, authToken, onComplete) {
-        return () => httpPost(endpoint, filecontents, authToken).then(onComplete);
+    post(endpoint, filecontents, onComplete) {
+        return () => httpPost(endpoint, filecontents).then(onComplete);
     }
 
     fire() {

packages/openchs-android/src/framework/http/requests.js
@@ -3,8 +3,15 @@ import _ from 'lodash';
 import AuthenticationError from "../../service/AuthenticationError";
 import ServerError from "../../service/ServerError";
 import Config from '../Config';
+import BeanRegistry from "../bean/BeanRegistry";
+import AuthService from "../../service/AuthService";
 
 const ACCEPTABLE_RESPONSE_STATUSES = [200, 201];
+
+const getAuthToken = async  () => {
+    const authService = BeanRegistry.getService(AuthService);
+    return await authService.getAuthToken();
+};
 
 const fetchFactory = (endpoint, method = "GET", params, fetchWithoutTimeout) => {
     const processResponse = (response) => {
@@ -42,39 +49,42 @@ const makeHeader = (type) => new Map([['json', {
 
 const makeRequest = (type, opts = {}) => _.assignIn({...makeHeader(type), ...opts});
 
-const addAuthIfRequired = (request, authToken) => {
-    if (Config.ENV === 'dev' || _.isEmpty(authToken)) {
-        return _.merge({}, request, {headers: {"USER-NAME": authToken}});
-    } else {
-        return _.merge({}, request, {headers: {'AUTH-TOKEN': authToken}});
+const _addAuthIfRequired = async (request, bypassAuth) => {
+    if (bypassAuth) {
+        return request;
     }
-    return request;
-}
+    const token = await getAuthToken();
+    return Config.ENV === 'dev' ?
+        _.merge({}, request, {headers: {"USER-NAME": token}}) :
+        _.merge({}, request, {headers: {'AUTH-TOKEN': token}});
+};
 
-let _get = (endpoint, authToken) => {
+let _get = (endpoint, bypassAuth) => {
     General.logDebug('Requests', `GET: ${endpoint}`);
-    return fetchFactory(endpoint, "GET", addAuthIfRequired(makeHeader("json"), authToken))
+    return _addAuthIfRequired(makeHeader("json"), bypassAuth)
+        .then((headers) => fetchFactory(endpoint, "GET", headers))
         .then((response) => response.json(), Promise.reject)
 };
 
-let _getText = (endpoint, authToken) => {
+let _getText = (endpoint, bypassAuth) => {
     General.logDebug('Requests', `Calling getText: ${endpoint}`);
-    return fetchFactory(endpoint, "GET", addAuthIfRequired(makeHeader("text"), authToken), true)
+    return _addAuthIfRequired(makeHeader("text"), bypassAuth)
+        .then((headers) => fetchFactory(endpoint, "GET", headers , true))
         .then((response) => response.text(), Promise.reject)
 };
 
-let _post = (endpoint, file, authToken, fetchWithoutTimeout) => {
-    const params = addAuthIfRequired(makeRequest("json", {body: JSON.stringify(file)}), authToken);
+let _post = (endpoint, file, fetchWithoutTimeout, bypassAuth = false) => {
+    const params = _addAuthIfRequired(makeRequest("json", {body: JSON.stringify(file)}), bypassAuth);
     General.logDebug('Requests', `POST: ${endpoint}`);
-    return fetchFactory(endpoint, "POST", params, fetchWithoutTimeout)
+    return params.then((headers) => fetchFactory(endpoint, "POST", headers, fetchWithoutTimeout))
 };
 
 export let post = _post;
 
-export let get = (endpoint, authToken) => {
-    return _getText(endpoint, authToken);
+export let get = (endpoint, bypassAuth = false) => {
+    return _getText(endpoint, bypassAuth);
 };
 
-export let getJSON = (endpoint, authToken) => {
-    return _get(endpoint, authToken);
+export let getJSON = (endpoint, bypassAuth = false) => {
+    return _get(endpoint, bypassAuth);
 };

packages/openchs-android/src/service/AuthService.js
@@ -225,7 +225,7 @@ class AuthService extends BaseService {
         const settings = this.settingsService.getSettings();
         const serverURL = settings.serverURL;
         const url = `${serverURL}/cognito-details`;
-        return getJSON(url).then((authDetails) => {
+        return getJSON(url, true).then((authDetails) => {
             let newSettings = settings.clone();
             newSettings.poolId = authDetails.poolId;
             newSettings.clientId = authDetails.clientId;

packages/openchs-android/src/service/BackupRestoreRealm.js
@@ -8,7 +8,6 @@ import BaseService from "../service/BaseService";
 import MediaQueueService from "../service/MediaQueueService";
 import {get} from '../framework/http/requests';
 import SettingsService from "../service/SettingsService";
-import AuthService from "../service/AuthService";
 import MediaService from "./MediaService";
 import _ from 'lodash';
 import EntityService from "./EntityService";
@@ -39,7 +38,6 @@ export default class BackupRestoreRealmService extends BaseService {
         let destFile = `${FileSystem.getBackupDir()}/${fileName}`;
         let destZipFile = `${FileSystem.getBackupDir()}/${fileName}.zip`;
         let mediaQueueService = this.getService(MediaQueueService);
-        let authService = this.getService(AuthService);
         General.logInfo("BackupRestoreRealmService", `Dest: ${destFile}`);
         this.db.writeCopyTo(destFile);
         zip(destFile, destZipFile)
@@ -47,8 +45,7 @@ export default class BackupRestoreRealmService extends BaseService {
                 General.logDebug("BackupRestoreRealmService", "Getting upload location");
                 cb(10, "backupUploading");
             })
-            .then(() => authService.getAuthToken())
-            .then((authToken) => mediaQueueService.getDumpUploadUrl(dumpType, authToken, `adhoc-dump-as-zip-${General.randomUUID()}`))
+            .then(() => mediaQueueService.getDumpUploadUrl(dumpType, `adhoc-dump-as-zip-${General.randomUUID()}`))
             .then((url) => mediaQueueService.foregroundUpload(url, destZipFile, (written, total) => {
                 General.logDebug("BackupRestoreRealmService", `Upload in progress ${written}/${total}`);
                 cb(10 + (97 - 10) * (written / total), "backupUploading");
@@ -71,7 +68,6 @@ export default class BackupRestoreRealmService extends BaseService {
     }
 
     restore(cb) {
-        let authService = this.getService(AuthService);
         let settingsService = this.getService(SettingsService);
         let mediaService = this.getService(MediaService);
         let downloadedFile = `${fs.DocumentDirectoryPath}/${General.randomUUID()}.zip`;
@@ -79,19 +75,15 @@ export default class BackupRestoreRealmService extends BaseService {
 
         General.logInfo("BackupRestoreRealm", `To be downloaded file: ${downloadedFile}, Unzipped directory: ${downloadedUncompressedDir}, Realm file: ${REALM_FILE_FULL_PATH}`);
 
-        return authService.getAuthToken()
-            .then((authToken) => {
-                cb(1, "restoreCheckDb")
-                return get(`${settingsService.getSettings().serverURL}/media/mobileDatabaseBackupUrl/exists`, authToken)
-            })
+        cb(1, "restoreCheckDb");
+        return get(`${settingsService.getSettings().serverURL}/media/mobileDatabaseBackupUrl/exists`)
             .then((exists) => {
                 General.logDebug("BackupRestoreRealmService", `Backup file exists:${exists}`);
                 if (exists === "true") {
-                    authService.getAuthToken()
-                        .then((authToken) => get(`${settingsService.getSettings().serverURL}/media/mobileDatabaseBackupUrl/download`, authToken))
+                    get(`${settingsService.getSettings().serverURL}/media/mobileDatabaseBackupUrl/download`)
                         .then((url) => mediaService.downloadFromUrl(url, downloadedFile, (received, total) => {
-                                cb(1 + (received * 85) / total, "restoreDownloadPreparedDb")
-                            }))
+                            cb(1 + (received * 85) / total, "restoreDownloadPreparedDb")
+                        }))
                         .then(() => {
                             General.logDebug("BackupRestoreRealmService", "Decompressing downloaded database dump");
                             cb(87, "restoringDb");

packages/openchs-android/src/service/MediaQueueService.js
@@ -76,19 +76,19 @@ class MediaQueueService extends BaseService {
         return `${directory}/${mediaQueueItem.fileName}`;
     }
 
-    getDumpUploadUrl(dumpType, authToken, fileName) {
+    getDumpUploadUrl(dumpType, fileName) {
         if (dumpType === MediaQueueService.DumpType.Catchment)
-            return get(`${this.getServerUrl()}/media/mobileDatabaseBackupUrl/upload`, authToken);
+            return get(`${this.getServerUrl()}/media/mobileDatabaseBackupUrl/upload`);
         else if (dumpType === MediaQueueService.DumpType.Adhoc)
-            return get(`${this.getServerUrl()}/media/uploadUrl/${fileName}`, authToken);
+            return get(`${this.getServerUrl()}/media/uploadUrl/${fileName}`);
     }
 
-    getUploadUrl(mediaQueueItem, auth) {
-        return this.getUploadUrlForFile(mediaQueueItem.fileName, auth);
+    getUploadUrl(mediaQueueItem) {
+        return this.getUploadUrlForFile(mediaQueueItem.fileName);
     }
 
-    getUploadUrlForFile(file, auth) {
-        return get(`${this.getServerUrl()}/media/uploadUrl/${file}`, auth);
+    getUploadUrlForFile(file) {
+        return get(`${this.getServerUrl()}/media/uploadUrl/${file}`);
     }
 
     mediaExists(mediaQueueItem) {
@@ -141,7 +141,7 @@ class MediaQueueService extends BaseService {
         }
     }
 
-    async uploadMediaQueueItem(mediaQueueItem, auth) {
+    async uploadMediaQueueItem(mediaQueueItem) {
         // Media can get deleted from the system by a user action.
         // The system should still work with missing media.
         // However, we need to find some way of highlighting this to user.
@@ -153,7 +153,7 @@ class MediaQueueService extends BaseService {
 
         let uploadUrl = "";
 
-        return this.getUploadUrl(mediaQueueItem, auth)
+        return this.getUploadUrl(mediaQueueItem)
             .then((url) => {
                 uploadUrl = url
             })
@@ -170,14 +170,14 @@ class MediaQueueService extends BaseService {
         return this.findAll().length > 0;
     }
 
-    uploadMedia(auth) {
+    uploadMedia() {
         // Parallel push to S3 ensures maximal usage of existing bandwidth.
         // Return only once everything is complete. Errors are logged in console only
         const mediaQueueItems = _.map(this.findAll(), (mediaQueueItem) => mediaQueueItem.clone());
         return Promise.all(
             _.map(mediaQueueItems,
                 (mediaQueueItem) => {
-                    return this.uploadMediaQueueItem(mediaQueueItem, auth)
+                    return this.uploadMediaQueueItem(mediaQueueItem)
                 }
             )
         );

packages/openchs-android/src/service/MediaService.js
@@ -1,14 +1,13 @@
 import Service from "../framework/bean/Service";
 import BaseService from "./BaseService";
 import SettingsService from "./SettingsService";
-import AuthService from "./AuthService";
 import RNFetchBlob from "rn-fetch-blob";
 import General from "../utility/General";
 import {get} from "../framework/http/requests";
 import FileSystem from "../model/FileSystem";
 import fs from 'react-native-fs';
 
-@Service("authService")
+@Service("mediaService")
 class MediaService extends BaseService {
     constructor(db, context) {
         super(db, context);
@@ -16,15 +15,12 @@ class MediaService extends BaseService {
 
     init() {
         this.settingsService = this.getService(SettingsService);
-        this.authService = this.getService(AuthService);
     }
 
     downloadMedia(remoteFilePath, targetFilePath) {
-        const authService = this.getService(AuthService);
         const settingsService = this.getService(SettingsService);
         const serverUrl = settingsService.getSettings().serverURL;
-        return authService.getAuthToken()
-            .then(auth => get(`${serverUrl}/media/signedUrl?url=${remoteFilePath}`, auth))
+        return get(`${serverUrl}/media/signedUrl?url=${remoteFilePath}`)
             .then(downloadUrl => this.downloadFromUrl(downloadUrl, targetFilePath));
     }
 

commit bd96bf393d4f6f205c54083886da91ecaea86876

fix the issue caused by previous commit 238fbbba23ae1b0d8ba137a47bf2ebe905c1a0b6

packages/openchs-android/src/service/SyncService.js
@@ -145,7 +145,7 @@ class SyncService extends BaseService {
         const allReferenceDataMetaData = allEntitiesMetaData.filter((entityMetaData) => entityMetaData.type === "reference");
         const allTxEntityMetaData = allEntitiesMetaData.filter((entityMetaData) => entityMetaData.type === "tx");
 
-        return statusMessageCallBack("uploadLocallySavedData")
+        return Promise.resolve(statusMessageCallBack("uploadLocallySavedData"))
             .then(() => this.pushData(allTxEntityMetaData.slice(), onProgressPerEntity))
             .then(() => onAfterMediaPush("After_Media", 0))
 