b7a8c1e8735e13bd08637742963549414c3745de
Vinod Deolal <vinoddeolal@gmail.com>
2022-01-17 13:09:26 +0530

    #549 | increase the line height for telgu language some text gets trimmed when language is telgu and text covers more than 3 lines

--- packages/openchs-android/src/views/primitives/PresetOptionItem.js
+++ packages/openchs-android/src/views/primitives/PresetOptionItem.js
@@ -8,6 +8,7 @@ import _ from 'lodash';
 import Styles from "./Styles";
 import themes from "./themes"
 import General from "../../utility/General";
+import UserInfoService from "../../service/UserInfoService";
 
 class PresetOptionItem extends AbstractComponent {
 
@@ -78,13 +79,16 @@ class PresetOptionItem extends AbstractComponent {
             container: this.props.style
         };
         const ToRender = this.props.chunked ? chunked : single;
+        const currentLocale = this.getService(UserInfoService).getUserSettings().locale;
+        const isExtraHeightRequired = _.includes(['te_IN'], currentLocale);
+        const extraLineHeight = isExtraHeightRequired ? {lineHeight: 20} : {};
         return (
             <TouchableOpacity onPress={() => this.onPress()} style={ToRender.container} disabled={this.props.disabled}>
                 <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start', alignSelf: 'flex-start'}}>
                     <View>
                         {this.getSelectComponent()}
                     </View>
-                    <Text style={[Styles.formBodyText, inputTextStyle, {color: color}]}>
+                    <Text style={[Styles.formBodyText, inputTextStyle, {color: color}, extraLineHeight]}>
                         {this.props.displayText}
                     </Text>
                 </View>
