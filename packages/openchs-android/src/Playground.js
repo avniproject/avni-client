4b2439b181733dd2265402d8b6fcf2bb62148a40
Vivek Singh <petmongrels@gmail.com>
2022-08-01 16:37:10 +0530

    #000 RN upgrade. Reproducing realm issue.

--- packages/openchs-android/src/Playground.js
+++ packages/openchs-android/src/Playground.js
@@ -1,72 +1,105 @@
-import PropTypes from 'prop-types';
 import React, {Component} from 'react';
-import BeanRegistry from './framework/bean/BeanRegistry';
 import Realm from 'realm';
-import {createRealmConfig, EntityMetaData} from 'openchs-models';
-import './views';
-import EntitySyncStatusService from "./service/EntitySyncStatusService";
-import themes from "./views/primitives/themes";
 import CHSContainer from "./views/common/CHSContainer";
 import CHSContent from "./views/common/CHSContent";
 import {Text} from "react-native";
-import {createStore} from "redux";
+import InitialSettings from "../config/initialSettings.json";
+import Config from "./framework/Config";
+import {Settings, LocaleMapping} from "openchs-models";
 
-let beans, mockStore, db = undefined;
+let db = undefined;
+
+// class Settings {
+//     static UUID = "875f2255-d9f2-4eb6-841b-dd1d537aa512";
+//     uuid;
+//     serverURL;
+//     locale;
+//     logLevel;
+//     pageSize;
+//     poolId;
+//     clientId;
+//     devSkipValidation;
+//     captureLocation;
+//     userId;
+//
+//     static schema = {
+//         name: "Settings",
+//         primaryKey: "uuid",
+//         properties: {
+//             uuid: "string",
+//             serverURL: "string",
+//             locale: { type: "LocaleMapping" },
+//             logLevel: "int",
+//             pageSize: "int",
+//             poolId: "string",
+//             clientId: "string",
+//             devSkipValidation: {type: "bool", default: false},
+//             captureLocation: {type: "bool", default: true},
+//             userId: {type: "string", optional: true},
+//         },
+//     };
+// }
+//
+// class LocaleMapping {
+//     static schema = {
+//         name: "LocaleMapping",
+//         primaryKey: "uuid",
+//         properties: {
+//             uuid: "string",
+//             locale: "string",
+//             displayText: "string",
+//         },
+//     };
+// }
+
+export const createRealmConfig = function () {
+    return {
+        //order is important, should be arranged according to the dependency
+        schema: [Settings, LocaleMapping],
+        schemaVersion: 1,
+        migration: () => {}
+    }
+};
 
 export default class App extends Component {
     constructor(props, context) {
         super(props, context);
         if (db === undefined) {
             db = new Realm(createRealmConfig());
-            beans = BeanRegistry.init(db);
-            mockStore = createStore((state, action) => this.mockReducer(state, action), {});
-            const entitySyncStatusService = beans.get(EntitySyncStatusService);
-            entitySyncStatusService.setup(EntityMetaData.model());
-        }
-        this.getBean = this.getBean.bind(this);
-    }
-
-    //This mock reducer can be used to perform any specific state changes you need to test out a component
-    mockReducer(state, action) {
-        switch (action.type) {
-            case 'AN_ACTION_NAME': {
-                this.setState({value: action.value});
-            }
         }
     }
 
-    static childContextTypes = {
-        getService: PropTypes.func.isRequired,
-        getDB: PropTypes.func.isRequired,
-        getStore: PropTypes.func.isRequired,
-        navigator: PropTypes.func.isRequired,
-    };
-
-    handleError(error, stacktrace) {
-        this.setState({error, stacktrace});
-    }
-
-    //Note that the store and navigator are mocked in this implementation
-    getChildContext = () => ({
-        getDB: () => db,
-        getService: (serviceName) => {
-            return beans.get(serviceName)
-        },
-        getStore: () => mockStore,
-        navigator: () => null
-    });
-
-    getBean(name) {
-        return beans.get(name);
+    createIfNotExists() {
+        if (db.objects(Settings).length < 1) {
+            db.write(() => {
+                const settings = new Settings();
+                settings.uuid = Settings.UUID;
+                settings.password = "";
+                settings.logLevel = InitialSettings.logLevel;
+                settings.pageSize = InitialSettings.pageSize;
+                settings.serverURL = Config.SERVER_URL;
+                settings.poolId = "";
+                settings.clientId = Config.CLIENT_ID || "";
+                db.create('Settings', settings, true);
+            });
+        }
     }
 
     render() {
+        this.createIfNotExists();
+        const objects = db.objects(Settings);
+
         return (<CHSContainer>
             <CHSContent>
                 <Text>
                     This is your playground to try out new components.
-                    You can go to the default app by adding playground=false in your .env file.
+                    You can go to the default app by adding PLAYGROUND=false in your .env file.
                 </Text>
+                <Text>objects.length</Text>
+                <Text>{objects.length}</Text>
+
+                <Text>setting.uuid</Text>
+                <Text>{objects[0].uuid}</Text>
             </CHSContent>
         </CHSContainer>)
     }
