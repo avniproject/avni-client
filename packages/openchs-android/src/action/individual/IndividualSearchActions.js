b41810d534da9fbd52a98839ee724a0c3d6ee4ed
Suhas Vishwanath <suhasv@samanvayfoundation.org>
2023-06-27 12:45:16 +0530

    #1020 | Fix selected gender toggle

--- packages/openchs-android/src/action/individual/IndividualSearchActions.js
+++ packages/openchs-android/src/action/individual/IndividualSearchActions.js
@@ -8,6 +8,7 @@ import _ from "lodash";
 import PrivilegeService from "../../service/PrivilegeService";
 import {firebaseEvents, logEvent} from "../../utility/Analytics";
 import AddressLevelState from "../common/AddressLevelsState";
+import {ArrayUtil} from "openchs-models";
 
 export class IndividualSearchActions {
     static clone(state) {
@@ -148,7 +149,9 @@ export class IndividualSearchActions {
     }
 
     static genderChange(state, action) {
-        return {...state, selectedGenders: action.selectedGenders};
+        let selectedGenders = state.selectedGenders || [];
+        ArrayUtil.toggle(selectedGenders, action.selectedGender, (l,r) => l.uuid === r.uuid)
+        return {...state, selectedGenders};
     }
 
     static loadIndicator(state, action) {
