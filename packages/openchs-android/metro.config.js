8f5118a32e818e5b77859551d858e02cdaf309bd
Joy A <itsjoy@gmail.com>
2023-08-08 11:50:30 +0530

    avniproject/avni-security#1 | Revert metro.config.js to pre-upgrade version - Also removed @react-native/metro-config dependency - This change was causing some beans to be loaded lazily (possibly due to https://github.com/react-native-community/discussions-and-proposals/blob/main/proposals/0605-lazy-bundling.md) which was causing issues in the app.

--- packages/openchs-android/metro.config.js
+++ packages/openchs-android/metro.config.js
@@ -1,23 +1,23 @@
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
 var path = require("path");
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-const config = {
-  resolver: {
-    extraNodeModules: {
-      "avni-models": path.resolve(__dirname, "node_modules/openchs-models"),
-    }
-  },
 
-  projectRoot: path.resolve(__dirname),
-  watchFolders: [],
+module.exports = {
+    transformer: {
+        getTransformOptions: async () => ({
+            transform: {
+                experimentalImportSupport: false,
+                inlineRequires: false,
+            },
+        }),
+    },
 
-  resetCache: true
+    resolver: {
+        extraNodeModules: {
+            "avni-models": path.resolve(__dirname, "node_modules/openchs-models"),
+        }
+    },
+
+    projectRoot: path.resolve(__dirname),
+    watchFolders: [],
+
+    resetCache: true
 };
-
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
-
