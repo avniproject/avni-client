5876d9f6759df5c8045c5bcf3c5533497e1e5479
Maha Lakshme S <mahalakshme1994@gmail.com>
2023-08-11 14:06:31 +0530

    avniproject/avni-security#2 | Obfuscate javascript

--- packages/openchs-android/metro.config.js
+++ packages/openchs-android/metro.config.js
@@ -1,5 +1,24 @@
 var path = require("path");
 
+const jsoMetroPlugin = require("obfuscator-io-metro-plugin")(
+    {
+        // for these option look javascript-obfuscator library options from  above url
+        compact: false,
+        sourceMap: true, // source Map generated after obfuscation is not useful right now so use default value i.e. false
+        controlFlowFlattening: true,
+        controlFlowFlatteningThreshold: 1,
+        numbersToExpressions: true,
+        simplify: true,
+        stringArrayShuffle: true,
+        splitStrings: true,
+        stringArrayThreshold: 1,
+    },
+    {
+        runInDev: false /* optional */,
+        logObfuscatedFiles: true /* optional generated files will be located at ./.jso */,
+    }
+);
+
 module.exports = {
     transformer: {
         getTransformOptions: async () => ({
@@ -9,7 +28,7 @@ module.exports = {
             },
         }),
     },
-
+    ...jsoMetroPlugin,
     resolver: {
         extraNodeModules: {
             "avni-models": path.resolve(__dirname, "node_modules/openchs-models"),
