c25cfc4382d3885d254f47598a79594cb8dfc322
Vivek Singh <petmongrels@gmail.com>
2023-09-05 11:26:48 +0530

    #1079 - fixing - switched index to main app

--- packages/openchs-android/index.android.js
+++ packages/openchs-android/index.android.js
@@ -1,4 +1,4 @@
 /* @flow */
 import {AppRegistry} from 'react-native';
-import App from "./integrationTest/IntegrationTestApp";
+import App from "./src/Avni";
 AppRegistry.registerComponent('Avni', () => App);
