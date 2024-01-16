704e3886780b83ffed5528644e9280a8857d9c6a
Vivek Singh <petmongrels@gmail.com>
2024-01-16 12:54:39 +0530

    #1254 - gradle build fix. fixed failing test. added test to reproduce problem with manual edit on calculated field. model update with removed fields on ObsHolder.

--- packages/openchs-android/test/model/form/TestFormElementFactory.js
+++ packages/openchs-android/test/model/form/TestFormElementFactory.js
@@ -2,7 +2,7 @@ import {FormElement} from 'openchs-models';
 import General from "../../../src/utility/General";
 
 class TestFormElementFactory {
-    static create({uuid = General.randomUUID(), name = General.randomUUID(), displayOrder, concept, formElementGroup, mandatory = true, keyValues = []}) {
+    static create({uuid = General.randomUUID(), name = General.randomUUID(), displayOrder, concept, formElementGroup, mandatory = true, keyValues = [], rule}) {
         const entity = new FormElement();
         entity.uuid = uuid;
         entity.name = name;
@@ -10,6 +10,7 @@ class TestFormElementFactory {
         entity.displayOrder = displayOrder;
         entity.mandatory = mandatory;
         entity.keyValues = keyValues;
+        entity.rule = rule;
         formElementGroup.addFormElement(entity);
         return entity;
     }
