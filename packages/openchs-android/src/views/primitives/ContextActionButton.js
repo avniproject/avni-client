9464000380cc4af1e98bfe36dc06d674fe1fb7d4
himeshr <himeshl@samanvayfoundation.org>
2024-05-23 18:40:09 +0530

    #1402 | Fix ripple on action buttons

--- packages/openchs-android/src/views/primitives/ContextActionButton.js
+++ packages/openchs-android/src/views/primitives/ContextActionButton.js
@@ -5,7 +5,7 @@ import {Text} from "native-base";
 import Colors from "../primitives/Colors";
 import Fonts from "../primitives/Fonts";
 import Styles from "./Styles";
-import {TouchableNativeFeedback} from "react-native";
+import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
 
 class ContextActionButton extends AbstractComponent {
     static propTypes = {
@@ -22,14 +22,15 @@ class ContextActionButton extends AbstractComponent {
     render() {
         const color = this.props.textColor || Colors.ActionButtonColor;
         return (
-            <TouchableNativeFeedback onPress={() => this.props.onPress()} style={{paddingHorizontal: 10}}>
+            <Pressable onPress={() => this.props.onPress()} style={{paddingHorizontal: 10}}
+                       android_ripple={{borderless: false, radius: 20, color}}>
                 <Text style={{
                     fontSize: Fonts.Medium,
                     color: color,
                     paddingHorizontal: 5,
                     backgroundColor: Styles.greyBackground,
                     borderRadius: 5
-                }}>{`${this.I18n.t(this.props.labelKey)}`}</Text></TouchableNativeFeedback>
+                }}>{`${this.I18n.t(this.props.labelKey)}`}</Text></Pressable>
         );
     }
 }
