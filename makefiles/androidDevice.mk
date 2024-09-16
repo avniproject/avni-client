a0d37b913fa5b993b41fe2545f13d4292a07dc34
Vivek Singh <petmongrels@gmail.com>
2024-09-16 14:40:28 +0530

    #0 - make targets with hyphen alias

--- makefiles/androidDevice.mk
+++ makefiles/androidDevice.mk
@@ -8,6 +8,7 @@ endef
 
 uninstall_apk:
 	-adb uninstall ${app_android_package_name}
+uninstall-apk: uninstall_apk
 
 clear_app_data:
 	-adb shell pm clear ${app_android_package_name}
@@ -28,11 +29,14 @@ endef
 
 kill_app:
 	$(call _kill_app,${app_android_package_name})
+kill-app: kill_app
 
 start_app:
 	$(call _start_app)
+start-app: start_app
 
 restart_app: kill_app start_app
+restart-app: restart_app
 
 open_playstore_openchs:
 	$(call _kill_app,com.google.android.gms)
@@ -58,17 +62,24 @@ run_app: setup_hosts as_dev _run_app
 run_app_release: as_dev _run_app_release
 
 run_app_staging: as_staging _run_app
+run-app-staging: run_app_staging
 run_app_staging_dev: as_staging_dev _run_app
-run_app_uat: as_uat _run_app
+run-app-staging-dev: run_app_staging_dev
 run_app_perf: as_perf _run_app
 run_app_prerelease: as_prerelease _run_app
+run-app-prerelease: run_app_prerelease
 run_app_prerelease_dev: as_prerelease_dev _run_app
+run-app-prerelease-dev: run_app_prerelease_dev
 run_app_prod: as_prod _run_app
+run-app-prod: run_app_prod
 run_app_prod_dev: as_prod_dev _run_app
+run-app-prod-dev: run_app_prod_dev
 run_app_no_env: as_no_env _run_app
 run_app_prod_lfe_dev: as_prod_lfe_dev _run_app
 run_app_prod_gramin_dev: as_prod_gramin_dev _run_app
+run-app-prod-gramin-dev: run_app_prod_gramin_dev
 run_app_staging_gramin_dev: as_staging_gramin_dev _run_app
+run-app-staging-gramin-dev: run_app_staging_gramin_dev
 
 stop_app:
 	adb shell am force-stop ${app_android_package_name}
