58b4e8d7b08c88f74601b62771f9c35ef3e2d0df
Vivek Singh <petmongrels@gmail.com>
2024-09-23 18:55:45 +0530

    avniproject/avni-models#53 - used for timezone verification code

--- packages/openchs-android/test/DateSpikeTest.js
+++ packages/openchs-android/test/DateSpikeTest.js
@@ -4,18 +4,37 @@ const scheduledOrCompletedEncountersOfType = function (nextVisitDate) {
     const nextDateMonth = moment(nextVisitDate).month();
     const nextDateYear = moment(nextVisitDate).year();
 
-    const data = [{earliestVisitDateTime: new Date("2024-06-30T18:30:00.000Z")}].filter((enc) =>
+    return [
+        {earliestVisitDateTime: new Date("2024-06-30T18:30:00.000Z")},
+        {earliestVisitDateTime: new Date("2024-07-31T18:30:00.000Z")},
+        {earliestVisitDateTime: new Date("2024-08-31T18:30:00.000Z")},
+    ].filter((enc) =>
         moment(enc.earliestVisitDateTime).year() === nextDateYear &&
         moment(enc.earliestVisitDateTime).month() === nextDateMonth
     );
-    return data;
 };
 
-it('should ', function () {
-    const date = new Date("2024-06-30T18:30:00.000Z");
-    // console.log(moment(date).month());
-    // console.log(scheduledOrCompletedEncountersOfType(date));
-    let date2 = moment(date).add(1, 'M').startOf('month').toDate();
-    console.log(date2);
-    // console.log("to server", JSON.stringify(date2));
+xit('should ', function () {
+    // console.log("nextDate", moment(new Date("2024-08-31T18:30:00.000Z")).add(1, 'M').startOf('month').toDate());
+    // console.log("nextDate", moment(new Date("2024-08-21T18:30:00.000Z")).add(1, 'M').startOf('month').toDate());
+
+    // perform, should schedule next visit
+    let currentEncountersEarliestDate = new Date("2024-08-31T18:30:00.000Z");
+    let nextDate = moment(currentEncountersEarliestDate).add(1, 'M').startOf('month').toDate();
+    console.log("found visits", scheduledOrCompletedEncountersOfType(nextDate).length, "Next date", nextDate);
+
+    currentEncountersEarliestDate = new Date("2024-08-31T18:30:00.000Z") || new Date("2024-09-05T10:30:00.000Z");
+    nextDate = moment(currentEncountersEarliestDate).add(1, 'M').startOf('month').toDate();
+    console.log("found visits", scheduledOrCompletedEncountersOfType(nextDate).length, "Next date", nextDate);
+
+    currentEncountersEarliestDate = new Date("2024-07-31T18:30:00.000Z");
+    nextDate = moment(currentEncountersEarliestDate).add(1, 'M').startOf('month').toDate();
+    console.log("found visits", scheduledOrCompletedEncountersOfType(nextDate).length, "Next date", nextDate);
+
+    console.log("Month", moment(new Date("2024-08-31T18:30:00.000Z")).format("MMMM"));
+    console.log("Month", moment(new Date()).month());
+
+    console.log("Month", moment(new Date("2024-01-20T18:30:00.000Z")).format("MMMM"));
+
+    console.log("timezone", new Date().getTimezoneOffset());
 });
