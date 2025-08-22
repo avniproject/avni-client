ee9774ed9d129d2ed46164974e57936f4ce74be5
Maha Lakshme S <mahalakshme1994@gmail.com>
2025-08-22 12:59:49 +0530

    #1714 | Change update mode to prevent dangling observations on sync

--- packages/openchs-android/src/service/BaseService.js
+++ packages/openchs-android/src/service/BaseService.js
@@ -112,7 +112,7 @@ class BaseService {
 
     getCreateEntityFunctions(schema, entities) {
         return entities.map((entity) => () => {
-            this.db.create(schema, entity, true)
+            this.db.create(schema, entity, Realm.UpdateMode.Modified)
         });
     }
 
