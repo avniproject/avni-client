f6c8a30a8645d48bf1050c8d72a05fe026e1ba13
himeshr <himeshl@samanvayfoundation.org>
2025-11-26 12:35:05 +0530

    Revert "#1751 | fixed QuestionGroup fragment key by assigning stable UUIDs instead of array index based keys which change when items are removed"
    This reverts commit 39bdd9759b7848e4f1577dc4fe3de85f715378e7.

--- packages/openchs-android/src/views/form/formElement/RepeatableFormElement.js
+++ packages/openchs-android/src/views/form/formElement/RepeatableFormElement.js
@@ -8,7 +8,6 @@ import {QuestionGroup as QuestionGroupModel, RepeatableQuestionGroup} from 'avni
 import QuestionGroup from "./QuestionGroup";
 import FormElementLabelWithDocumentation from "../../common/FormElementLabelWithDocumentation";
 import _ from "lodash";
-import General from "../../../utility/General";
 
 class RepeatableFormElement extends AbstractFormElement {
 
@@ -64,12 +63,8 @@ class RepeatableFormElement extends AbstractFormElement {
 
     renderQuestionGroup(questionGroupIndex) {
         const isRemoveDisabled = this.props.value.size() <= 1;
-        const questionGroupObs = this.props.value.getGroupObservationAtIndex(questionGroupIndex) || new QuestionGroupModel();
-        
-        questionGroupObs.uuid = questionGroupObs.uuid || General.randomUUID();
-        
         return (
-            <Fragment key={questionGroupObs.uuid}>
+            <Fragment key={questionGroupIndex}>
                 {this.actionButton('minus-circle', () => this.onRemove(questionGroupIndex), isRemoveDisabled, Colors.NegativeActionButtonColor)}
                 <QuestionGroup
                     questionGroupIndex={questionGroupIndex}
@@ -78,7 +73,7 @@ class RepeatableFormElement extends AbstractFormElement {
                     actions={this.props.actions}
                     formElementsUserState={this.props.formElementsUserState}
                     observationHolder={this.props.observationHolder}
-                    value={questionGroupObs}
+                    value={this.props.value.getGroupObservationAtIndex(questionGroupIndex) || new QuestionGroupModel()}
                     validationResults={this.props.validationResults}
                     filteredFormElements={this.props.filteredFormElements}
                     extraContainerStyle={{marginVertical: 0}}
